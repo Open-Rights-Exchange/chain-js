b3331ce5c2b6d6143f143e6d2fa21e6c59cb695e
Tray Lewin <traylewin@Trays-iMac.local>
2020-01-11 17:26:33 -0800

    export crypto on chain

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import * as crypto from './crypto'
 import { ChainFactory } from './chainFactory'
 import { ChainType } from './models'
 import { Account, Chain, CreateAccount, Transaction } from './interfaces'
 import { ChainEosV18 } from './chains/eos_1_8/ChainEosV18'
 import { ChainEthereumV1 } from './chains/ethereum_1/ChainEthereumV1'
 
-export { Account, Chain, ChainEosV18, ChainEthereumV1, ChainFactory, CreateAccount, Transaction, ChainType }
+export { Account, Chain, ChainEosV18, ChainEthereumV1, ChainFactory, CreateAccount, crypto, Transaction, ChainType }
