207b92e2069a5f1bcc01c55ba49cd970248be16a
Tray Lewin <traylewin@Trays-iMac.local>
2020-01-19 10:41:23 -0800

    export chain plug-in types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as crypto from './crypto'
 import { ChainFactory } from './chainFactory'
 import { Account, Chain, CreateAccount, Transaction } from './interfaces'
+import * as EosV18 from './chains/eos_1_8'
 import { ChainEosV18 } from './chains/eos_1_8/ChainEosV18'
 import { ChainEthereumV1 } from './chains/ethereum_1/ChainEthereumV1'
+import * as Ethereum10 from './chains/ethereum_1'
 import * as Models from './models'
 import * as ModelsEos from './chains/eos_1_8/models'
 import * as ModelsEthereum from './chains/ethereum_1/models'
@@ -18,6 +20,8 @@ export {
   ChainType,
   CreateAccount,
   crypto,
+  EosV18,
+  Ethereum10,
   Models,
   ModelsEos,
   ModelsEthereum,
