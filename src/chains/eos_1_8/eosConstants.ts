316da4f779489f48803ac9e4ca676f571023eb55
Tray Lewin <traylewin@Trays-iMac.local>
2020-02-21 11:14:21 -0800

    set DEFAULT_TRANSACTION_EXPIRY_IN_SECONDS = 300

--- src/chains/eos_1_8/eosConstants.ts
+++ src/chains/eos_1_8/eosConstants.ts
@@ -1,5 +1,5 @@
 // sign transaction default parameters
-export const DEFAULT_TRANSACTION_EXPIRY_IN_SECONDS = 30
+export const DEFAULT_TRANSACTION_EXPIRY_IN_SECONDS = 300 // 5 minutes
 export const DEFAULT_TRANSACTION_BLOCKS_BEHIND_REF_BLOCK = 3
 export const TRANSACTION_ENCODING = 'utf8'
 export const CHAIN_BLOCK_FREQUENCY = 0.5 // time between blocks produced in seconds
