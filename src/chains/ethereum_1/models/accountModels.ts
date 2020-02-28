ac4d0ef2a4561500a0506c934264c19807668ab4
Surabhi Lodha <surabhil@usc.edu>
2020-02-28 19:08:53 +0530

    add eth account creation option

--- src/chains/ethereum_1/models/accountModels.ts
+++ src/chains/ethereum_1/models/accountModels.ts
@@ -1,18 +1,10 @@
 /** Type of account to craate */
-export enum EthereumAccountType {
+export enum EthereumNewAccountType {
   /** Native account for chain tyep (EOS, Ethereum, etc.) */
   Native = 'Native',
 }
 
 export type EthereumCreateAccountOptions = {
-  // accountName: EthereumEntityName,
-  // creatorAccountName: EthereumEntityName
-  // creatorPermission: EthereumEntityName
-  /** to generate new keys (using newKeysOptions), leave both publicKeys as null */
-  publicKeys?: {
-    // owner?: EthereumPublicKey
-    // active?: EthereumPublicKey
-  }
   newKeysOptions?: {
     password?: string
     salt?: string
