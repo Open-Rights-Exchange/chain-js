af4d654cf2d75da3570ab7676738fb3567fe5af3
Surabhi Lodha <surabhil@usc.edu>
2020-02-28 16:33:49 +0530

    use eth util anf eth wallet modules for crypto helper functions

--- src/chains/ethereum_1/ethCrypto.ts
+++ src/chains/ethereum_1/ethCrypto.ts
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import ethUtil from 'ethereumjs-util'
-import Web3 from 'web3'
-// standard library for elliptic curve used by most blockchains (ethereum, bitcoin..)
-import secp256k1 from 'secp256k1'
-import { isAString, toBuffer } from '../../helpers'
-import { PublicKey, Signature } from '../../models'
+import Wallet from 'ethereumjs-wallet'
+import { toBuffer } from '../../helpers'
 import { throwNewError } from '../../errors'
-import { EthPublicKey, EthSignature } from './models/cryptoModels'
+import { EthAddress, EthPublicKey, EthSignature, EthPrivateKey } from './models/cryptoModels'
 import { toEthBuffer } from './helpers/generalHelpers'
+import { isEncryptedDataString, encrypt, toEncryptedDataString } from '../../crypto'
 
 export function sign(data: string | Buffer, privateKey: string): EthSignature {
   const dataBuffer = toEthBuffer(data)
@@ -15,12 +13,17 @@ export function sign(data: string | Buffer, privateKey: string): EthSignature {
   return ethUtil.ecsign(dataBuffer, keyBuffer)
 }
 
-export function isValidPrivateKey(value: string): boolean {
-  return secp256k1.privateKeyVerify(value)
+export function isValidPrivateKey(value: EthPrivateKey): boolean {
+  return ethUtil.isValidPrivate(value)
 }
 
 export function isValidPublicKey(value: EthPublicKey): boolean {
-  return Web3.utils.isAddress(value)
+  return ethUtil.isValidPublic(value)
+}
+
+// For a given private key, pr, the Ethereum address A(pr) (a 160-bit value) to which it corresponds is defined as the right most 160-bits of the Keccak hash of the corresponding ECDSA public key.
+export function isValidAddress(value: EthAddress): boolean {
+  return ethUtil.isValidAddress(value)
 }
 
 export function getPublicKeyFromSignature(
@@ -32,11 +35,28 @@ export function getPublicKeyFromSignature(
   return ethUtil.ecrecover(toEthBuffer(data), v, r, s)
 }
 
-export function generateNewAccountKeysAndEncryptPrivateKeys(password: string, salt: string, overrideKeys: any): any {
-  throwNewError('Not implemented')
-  return null
+/** Replaces unencrypted privateKey in keys object
+ *  Encrypts key using password and salt */
+export function encryptAccountPrivateKeysIfNeeded(keys: any, password: string, salt: string) {
+  const { privateKey, publicKey } = keys
+  const encryptedKeys = {
+    privateKey: isEncryptedDataString(privateKey) ? privateKey : encrypt(privateKey, password, salt).toString(),
+    publicKey,
+  }
+  return encryptedKeys
 }
 
+export function generateNewAccountKeysAndEncryptPrivateKeys(password: string, salt: string, overrideKeys: any): any {
+  const wallet = Wallet.generate()
+  const privateKey: EthPrivateKey = wallet.getPrivateKeyString()
+  // TODO: eth address is commonely referred to as public key. However there is a difference b/w those two. So should we just call eth address as public key to reduce confusion??
+  const publicKey: EthAddress = wallet.getAddressString()
+  const keys = { privateKey, publicKey }
+  const encryptedKeys = encryptAccountPrivateKeysIfNeeded(keys, password, salt)
+  return encryptedKeys
+}
+
+// TODO: unless the data is signature, not sure what is the purpose of the function.  And how is it possibel to verify that the signature has what public key if the original data is not known
 export function verifySignedWithPublicKey(
   publicKey: string | Buffer,
   data: string | Buffer,
