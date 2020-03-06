3afcec26036ac156e1c51833e068db00d8740116
Surabhi Lodha <surabhil@usc.edu>
2020-03-06 12:37:37 -0800

    update general helpers

--- src/chains/ethereum_1/helpers/generalHelpers.ts
+++ src/chains/ethereum_1/helpers/generalHelpers.ts
@@ -1,5 +1,5 @@
-import ethUtil from 'ethereumjs-util'
+import { toBuffer } from 'ethereumjs-util'
 
 export function toEthBuffer(data: string | Buffer): Buffer {
-  return ethUtil.toBuffer(data)
+  return toBuffer(data)
 }
