764d4b5099c3aeed78f10ddb6429724799b06afe
Surabhi Lodha <surabhil@usc.edu>
2020-03-06 11:53:57 -0800

    add public key to ethereum create account options

--- src/chains/ethereum_1/models/accountModels.ts
+++ src/chains/ethereum_1/models/accountModels.ts
@@ -1,12 +1,13 @@
-/** Type of account to craate */
+import { EthereumNewKeysOptions } from './generalModels'
+import { EthereumPublicKey } from './cryptoModels'
+
+/** Type of account to create */
 export enum EthereumNewAccountType {
-  /** Native account for chain tyep (EOS, Ethereum, etc.) */
+  /** Native account for chain type (Ethereum, etc.) */
   Native = 'Native',
 }
 
 export type EthereumCreateAccountOptions = {
-  newKeysOptions?: {
-    password?: string
-    salt?: string
-  }
+  publicKey: EthereumPublicKey
+  newKeysOptions?: EthereumNewKeysOptions
 }
