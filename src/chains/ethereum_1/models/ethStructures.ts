b5c80b4d4f464444ee854d0b94c55da4cf4bcbe2
Surabhi Lodha <surabhil@usc.edu>
2020-03-10 17:59:02 -0700

    add EthereumGeneratedKeys type

--- src/chains/ethereum_1/models/ethStructures.ts
+++
@@ -1,7 +0,0 @@
-import { EthereumPrivateKey, EthereumAddress, EthereumPublicKey } from './cryptoModels'
-
-export type EthereumAccountStruct = {
-  address: EthereumAddress
-  publicKey: EthereumPublicKey
-  privateKey: EthereumPrivateKey
-}
--- src/chains/ethereum_1/models/generalModels.ts
+++ src/chains/ethereum_1/models/generalModels.ts
@@ -1,8 +1,14 @@
-import { ChainEntityNameBrand } from '../../../models'
+import { EncryptedDataString, ChainEntityNameBrand } from '../../../models'
+import { EthereumPrivateKey, EthereumPublicKey } from './cryptoModels'
 
 export type EthereumEntityName = string & ChainEntityNameBrand
 
 export type EthereumNewKeysOptions = {
   password?: string
   salt?: string
+}
+
+export type EthereumGeneratedKeys = {
+  publicKey: EthereumPublicKey
+  privateKey: EthereumPrivateKey | EncryptedDataString
 }
