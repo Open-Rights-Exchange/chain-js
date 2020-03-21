f8e06687501acbeedca0784724abcb3daf6fe1f6
basarrcan <basarrcan@gmail.com>
2020-03-21 04:51:39 +0300

    eth token transfer compose refactor

--- src/chains/ethereum_1/ethAction.ts
+++ src/chains/ethereum_1/ethAction.ts
@@ -1,20 +1,17 @@
-import { AbiItem } from 'web3-eth-contract'
 import { isNullOrEmpty } from '../../helpers'
 import { EthereumActionInput, EthereumAddress, EthereumValue, EthereumContractAction } from './models'
 import { ethereumTrxArgIsNullOrEmpty, generateDataFromContractAction } from './helpers'
+import { ZERO_HEX, ZERO_ADDRESS } from '../../constants'
+import { throwNewError } from '../../errors'
 
 export class EthereumAction {
   private _data: any
-
-  private _abi: AbiItem[]
 
   private _to: EthereumAddress
 
   private _value: EthereumValue
 
   private _contract: EthereumContractAction
-
-  private _isValidated: boolean
 
   /** Creates a new Action from 'human-readable' transfer or contact info
    *  OR from 'raw' data property
@@ -23,10 +20,10 @@ export class EthereumAction {
     // if raw data is provided, use that to set the action properties
     const { to, value, contract, data } = action
 
-    this._to = isNullOrEmpty(to) ? '0x0000000000000000000000000000000000000000' : to
-    this._value = isNullOrEmpty(value) ? '0x00' : value
-
-    if (isNullOrEmpty(contract) && ethereumTrxArgIsNullOrEmpty(data)) this._data = '0x00'
+    this._to = isNullOrEmpty(to) ? ZERO_ADDRESS : to
+    this._value = isNullOrEmpty(value) ? ZERO_HEX : value
+    if (!ethereumTrxArgIsNullOrEmpty(data)) this._data = data
+    else if (isNullOrEmpty(contract) && ethereumTrxArgIsNullOrEmpty(data)) this._data = ZERO_HEX
     else if (!isNullOrEmpty(contract)) {
       this._contract = contract
       this._data = generateDataFromContractAction(contract)
@@ -40,6 +37,25 @@ export class EthereumAction {
 
   get hasData(): boolean {
     return !ethereumTrxArgIsNullOrEmpty(this._data)
+  }
+
+  // check if both data and to & value fields are empty, if so throw error
+  private validateActionBody(): void {
+    if (
+      ethereumTrxArgIsNullOrEmpty(this._data) &&
+      (ethereumTrxArgIsNullOrEmpty(this._to) || ethereumTrxArgIsNullOrEmpty(this._value))
+    )
+      throwNewError('Action is not valid.')
+  }
+
+  public getActionBody(): EthereumActionInput {
+    this.validateActionBody()
+    return {
+      to: this._to,
+      value: this._value,
+      data: this._data,
+      contract: this._contract,
+    }
   }
 
   public assertValidateData(data: any): boolean {
