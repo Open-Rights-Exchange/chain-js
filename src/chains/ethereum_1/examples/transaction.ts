3bd128554cd54717eb540c243d70667d4c07a1a3
basarrcan <basarrcan@gmail.com>
2020-04-08 00:45:48 +0300

    example lint error ignore

--- src/chains/ethereum_1/examples/transaction.ts
+++ src/chains/ethereum_1/examples/transaction.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Web3 from 'web3'
 import { ChainFactory, ChainType } from '../../../index'
 import { ChainActionType, ChainSettings, ChainEndpoint } from '../../../models'
