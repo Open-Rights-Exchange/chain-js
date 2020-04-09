5b3b2591fb4e57262a09bb35a8b9d22ef58677d3
basarrcan <basarrcan@gmail.com>
2020-04-09 20:00:28 +0300

    unit test UnitType enum fix

--- src/chains/ethereum_1/tests/helpers.test.ts
+++ src/chains/ethereum_1/tests/helpers.test.ts
@@ -1,11 +1,12 @@
 // How to use fetch mocks - https://www.npmjs.com/package/jest-fetch-mock
 import { toWei } from '../helpers'
+import { EthUnit } from '../models'
 
 describe('Ethereum Helper Functions', () => {
   // sets fetchMock to throw an error on the next call to fetch (jsonRpc.get_abi calls fetch and triggers the error to be thrown)
   it('tests toWei', async () => {
-    const expValue = toWei(100, 'milliether')
-    const value = toWei(100, 'finney')
+    const expValue = toWei(100, EthUnit.Milliether)
+    const value = toWei(100, EthUnit.Finney)
 
     expect(value).toEqual(expValue)
   })
