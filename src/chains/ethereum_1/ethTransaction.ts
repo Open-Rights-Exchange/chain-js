2611ec2793c05275e24b927ecc3ca443afd9b752
Tray Lewin <traylewin@Trays-iMac.local>
2020-04-10 15:42:30 -0700

    fix bugs and cleanup syntax

--- src/chains/ethereum_1/ethTransaction.ts
+++ src/chains/ethereum_1/ethTransaction.ts
@@ -19,7 +19,7 @@ import { throwNewError } from '../../errors'
 import { isNullOrEmpty } from '../../helpers'
 import {
   isValidEthereumSignature,
-  isLengthOne,
+  isArrayLengthOne,
   toEthereumSignature,
   toEthBuffer,
   addPrefixToHex,
@@ -32,8 +32,6 @@ import {
 import { EthereumActionHelper } from './ethAction'
 
 export class EthereumTransaction implements Transaction {
-  private _cachedAccounts: any[] = []
-
   private _actionHelper: EthereumActionHelper
 
   private _chainState: EthereumChainState
@@ -42,11 +40,14 @@ export class EthereumTransaction implements Transaction {
 
   private _options: EthereumTransactionOptions
 
-  private _signature: EthereumSignature // A set keeps only unique values
+  /** A set keeps only unique values */
+  private _signature: EthereumSignature
 
-  private _fromAddress: EthereumAddress // Address that retrieved from signature
+  /** Address retrieved from attached signature */
+  private _fromAddress: EthereumAddress
 
-  private _fromPublicKey: EthereumPublicKey // Public Key that retrieved from signature
+  /** Public Key retrieved from attached signature */
+  private _fromPublicKey: EthereumPublicKey
 
   /** Transaction prepared for signing (raw transaction) */
   private _raw: EthereumJsTx
@@ -157,33 +158,47 @@ export class EthereumTransaction implements Transaction {
     return { txAction: { to, value, data: toEthereumTxData(data) }, txHeader: { nonce, gasLimit, gasPrice } }
   }
 
-  /** Ethereum transfer & contract actions executed by the transaction */
+  /** Ethereum transaction action (transfer & contract functions)
+   * Returns null or an array with exactly one action
+   */
   public get actions(): EthereumTransactionAction[] {
-    if (!this._actionHelper.raw) return null
-    const action = { ...this._actionHelper.raw, contract: this._actionHelper.contract }
+    const { action } = this
+    if (!action) {
+      return null
+    }
     return [action]
   }
 
-  /** Sets the Array of actions.
+  /** Private property for the Ethereum contract action - uses _actionHelper */
+  private get action(): EthereumTransactionAction {
+    if (!this?._actionHelper?.raw) return null
+    const action = { ...this._actionHelper?.raw, contract: this._actionHelper?.contract }
+    return action
+  }
+
+  /** Sets actions array
    * Array length has to be exactly 1 because ethereum doesn't support multiple actions
    */
   public set actions(actions: EthereumTransactionAction[]) {
-    if (!isLengthOne(actions)) {
-      throwNewError('Ethereum set actions function only allows actions array length of 1')
-    }
     this.assertNoSignatures()
+    if (!isArrayLengthOne(actions)) {
+      throwNewError('Ethereum transaction.actions only accepts an array of exactly 1 action')
+    }
+    const action = actions[0]
     // eslint-disable-next-line prefer-destructuring
-    this._actionHelper = new EthereumActionHelper(actions[0])
+    this._actionHelper = new EthereumActionHelper(action)
     this._isValidated = false
   }
 
-  /** Add one action to the transaction body
-   *  Transaction's action has to be empty to be able to add an action
-   *  Therefore asFirstAction option does not affect behavior */
+  /** Add action to the transaction body
+   *  throws if transaction.actions already has a value
+   *  Ignores asFirstAction parameter since only one action is supported in ethereum */
   public addAction(action: EthereumTransactionAction, asFirstAction?: boolean): void {
     this.assertNoSignatures()
     if (!isNullOrEmpty(this._actionHelper)) {
-      throwNewError('addAction failed. Transaction already has an action. Ethereum only supports 1 action.')
+      throwNewError(
+        'addAction failed. Transaction already has an action. Use transaction.actions to replace existing action.',
+      )
     }
     this._actionHelper = new EthereumActionHelper(action)
     this._isValidated = false
@@ -191,7 +206,7 @@ export class EthereumTransaction implements Transaction {
 
   // validation
 
-  /** Verifies that raw trx exist.
+  /** Verifies that raw trx exists
    *  Throws if any problems */
   public async validate(): Promise<void> {
     if (!this.hasRaw) {
@@ -226,29 +241,27 @@ export class EthereumTransaction implements Transaction {
 
   /** Sets the Set of signatures */
   set signatures(signatures: EthereumSignature[]) {
-    if (!isLengthOne(signatures)) {
+    if (!isArrayLengthOne(signatures)) {
       throwNewError('Ethereum set signatures function only allows signatures array length of 1')
     }
-    signatures.forEach(sig => {
-      this.assertValidSignature(sig)
-    })
-    // eslint-disable-next-line prefer-destructuring
-    this._signature = signatures[0]
+    const signature = signatures[0]
+    this.assertValidSignature(signature)
+    this._signature = signature
   }
 
-  /** Add signature to raw transaction.
-   * Array length has to be exactly 1
+  /** Add signature to raw transaction
+   * Accepts array with exactly one signature
    */
   addSignatures = (signatures: EthereumSignature[]): void => {
-    if (isLengthOne(signatures)) {
+    if (isArrayLengthOne(signatures)) {
       throwNewError('Ethereum addSignature function only allows signatures array length of 1')
     }
-    const { v, r, s } = signatures[0]
+    const signature = signatures[0]
+    const { v, r, s } = signature
     this._raw.v = toEthBuffer(v)
     this._raw.r = r
     this._raw.s = s
-    // eslint-disable-next-line prefer-destructuring
-    this._signature = signatures[0]
+    this._signature = signature
   }
 
   /** Throws if signatures isn't properly formatted */
@@ -256,6 +269,11 @@ export class EthereumTransaction implements Transaction {
     if (!isValidEthereumSignature(signature)) {
       throwNewError(`Not a valid signature : ${signature}`, 'signature_invalid')
     }
+  }
+
+  /** Whether there is an attached signature */
+  get hasAnySignatures(): boolean {
+    return !isNullOrEmpty(this.signatures)
   }
 
   /** Throws if any signatures are attached */
@@ -267,11 +285,6 @@ export class EthereumTransaction implements Transaction {
     }
   }
 
-  /** Whether there are any signatures attached */
-  get hasAnySignatures(): boolean {
-    return !isNullOrEmpty(this.signatures)
-  }
-
   /** Throws if transaction is missing any signatures */
   private assertHasSignature(): void {
     if (!this.hasAnySignatures) {
@@ -281,43 +294,53 @@ export class EthereumTransaction implements Transaction {
 
   /** Whether there is an attached signature for the provided publicKey */
   public hasSignatureForPublicKey = (publicKey: EthereumPublicKey): boolean => {
-    return this._fromPublicKey === publicKey
+    return this?._fromPublicKey === publicKey
   }
 
   /** Whether there is an attached signature for the publicKey of the address */
   public async hasSignatureForAuthorization(authorization: EthereumAddress): Promise<boolean> {
-    return this._fromAddress === authorization
+    return this?._fromAddress === authorization
   }
 
-  /** Checks if signature belongs to the "from" property of action.
-   *  Only callable if "from" is a valid address */
+  /** Whether signature is attached to transaction (and/or whether the signature is correct)
+   * If a specific action.from is specifed, ensure that attached signature matches its address/public key */
   public get hasAllRequiredSignatures(): boolean {
-    this.assertFromNotEmpty()
-    this.assertIsValidated()
-    return this.requiredAuthorizations[0] === this._fromAddress
+    // If a specific action.from is specifed, ensure that a signature is attached that matches its address/public key
+    if (this.isFromIsValidAddress()) {
+      return this.requiredAuthorization === this._fromAddress
+    }
+    // if no specific action.from, just confirm any signature is attached
+    return this.hasAnySignatures
   }
 
   /** Throws if transaction is missing any signatures */
   private assertHasAllRequiredSignature(): void {
+    // If a specific action.from is specifed, ensure that a signature is attached that matches its address/public key
     if (!this.hasAllRequiredSignatures) {
       throwNewError('Missing at least one required Signature', 'transaction_missing_signature')
     }
   }
 
-  /** Checks if transaction has the signature belongs to "from" property of action.
-   *  Only callable if "from" is a valid address */
-  public get missingSignatures(): any {
+  /** Returns address, for which, a matching signature must be attached to transaction
+   *  ... always an array of length 1 because ethereum only supports one signature
+   *  Throws if actions[].from is not a valid address - needed to determine the required signature */
+  public get missingSignatures(): EthereumAddress[] {
     this.assertIsValidated()
-    const missing = this.hasAllRequiredSignatures ? null : this.requiredAuthorizations[0]
-    return ethereumTrxArgIsNullOrEmpty(missing) ? null : missing // if no values, return null instead of empty array
+    const missingSignature = this.hasAllRequiredSignatures ? null : this.requiredAuthorization
+    return isNullOrEmpty(missingSignature) ? null : [missingSignature] // if no values, return null instead of empty array
   }
 
-  /** Returns address of the transaction sender defined in action's "from" property
-   *  Only callable if "from" is a valid address. */
+  /** Returns address specified by actions[].from property
+   * throws if actions[].from is not a valid address - needed to determine the required signature */
   public get requiredAuthorizations(): EthereumAddress[] {
+    return [this.requiredAuthorization]
+  }
+
+  /** private property for the one signature address required (by action.from) */
+  private get requiredAuthorization(): EthereumAddress {
     this.assertIsValidated()
-    this.assertFromNotEmpty()
-    return [this.actions[0].from]
+    this.assertFromIsValidAddress()
+    return this.action.from
   }
 
   public get signBuffer(): Buffer {
@@ -329,10 +352,11 @@ export class EthereumTransaction implements Transaction {
   /** Sign the transaction body with private key and add to attached signatures */
   public async sign(privateKeys: EthereumPrivateKey[]): Promise<void> {
     this.assertIsValidated()
-    if (privateKeys.length !== 1) {
-      throwNewError('Ethereum sign needs to be providen exactly 1 privateKey')
+    if (!isArrayLengthOne(privateKeys)) {
+      throwNewError('Ethereum sign() requires privateKeys array of length one')
     }
-    const privateKeyBuffer = toEthBuffer(toEthereumPrivateKey(privateKeys[0]))
+    const privateKey = privateKeys[0]
+    const privateKeyBuffer = toEthBuffer(toEthereumPrivateKey(privateKey))
     if (bufferToHex(this._raw.nonce) === EMPTY_HEX) {
       const addressBuffer = privateToAddress(privateKeyBuffer)
       const address = bufferToHex(addressBuffer)
@@ -353,16 +377,11 @@ export class EthereumTransaction implements Transaction {
 
   // send
 
-  // TODO add confirmation enum usage
   /** Broadcast a signed transaction to the chain
    *  waitForConfirm specifies whether to wait for a transaction to appear in a block before returning */
   public send(waitForConfirm: ConfirmType = ConfirmType.None): Promise<any> {
     this.assertIsValidated()
-    if (this.isFromDefined()) {
-      this.assertHasAllRequiredSignature()
-    } else {
-      this.assertHasSignature()
-    }
+    this.assertHasAllRequiredSignature()
     // Serialize the entire transaction for sending to chain (prepared transaction that includes signatures { v, r , s })
     const signedTransaction = this._raw.serialize()
 
@@ -385,14 +404,15 @@ export class EthereumTransaction implements Transaction {
     }
   }
 
-  private isFromDefined(): boolean {
-    return !ethereumTrxArgIsNullOrEmpty(this.actions[0]?.from)
+  /** Whether action.from is a valid address (and not null or empty e.g. '0x00') */
+  private isFromIsValidAddress(): boolean {
+    return !ethereumTrxArgIsNullOrEmpty(this?.action?.from)
   }
 
   /** Throws is from is not null or empty ethereum argument */
-  private assertFromNotEmpty(): void {
-    if (!this.isFromDefined()) {
-      throwNewError('Transaction doesnt have "from" property defined in action.')
+  private assertFromIsValidAddress(): void {
+    if (!this.isFromIsValidAddress()) {
+      throwNewError('Transaction action[].from is not a valid address.')
     }
   }
 
