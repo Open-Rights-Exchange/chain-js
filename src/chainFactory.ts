fb7f64b2b7db316c44e25e8a5dcd4907609353cb
Tray Lewin <traylewin@Trays-iMac.local>
2020-04-22 11:08:00 -0700

    ChainFactory settings now any - chains are diff

--- src/chainFactory.ts
+++ src/chainFactory.ts
@@ -1,14 +1,14 @@
 import { ChainEosV18 } from './chains/eos_1_8/ChainEosV18'
 import { ChainEthereumV1 } from './chains/ethereum_1/ChainEthereumV1'
 import { Chain } from './interfaces'
-import { ChainType, ChainEndpoint, ChainSettings } from './models'
+import { ChainType, ChainEndpoint } from './models'
 import { throwNewError } from './errors'
 
 /**
  * Returns an instance of one of the concrete chain classes
  */
 export class ChainFactory {
-  public create = (chainType: ChainType, endpoints: ChainEndpoint[], settings: ChainSettings): Chain => {
+  public create = (chainType: ChainType, endpoints: ChainEndpoint[], settings?: any): Chain => {
     switch (chainType) {
       case ChainType.EosV18:
         return new ChainEosV18(endpoints, settings)
