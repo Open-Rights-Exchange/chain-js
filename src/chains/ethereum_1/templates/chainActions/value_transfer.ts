b0842f2ee7d7f2b52114c977914c24aab4dd200c
Surabhi Lodha <surabhil@usc.edu>
2020-05-19 16:53:22 -0700

    add eth template for value transfer

---
+++ src/chains/ethereum_1/templates/chainActions/value_transfer.ts
@@ -0,0 +1,33 @@
+import { EthereumAddress, EthUnit, EthereumChainActionType, EthereumTransactionAction } from '../../models'
+import { toWei } from '../../helpers'
+import { DEFAULT_ETH_SYMBOL } from '../../ethConstants'
+
+interface valueTransferParams {
+  fromAccountName?: EthereumAddress
+  toAccountName: EthereumAddress
+  tokenAmount: number
+  tokenSymbol?: EthUnit
+}
+
+export const composeAction = ({
+  fromAccountName,
+  toAccountName,
+  tokenAmount,
+  tokenSymbol = DEFAULT_ETH_SYMBOL,
+}: valueTransferParams) => ({
+  from: fromAccountName,
+  to: toAccountName,
+  value: toWei(tokenAmount, tokenSymbol),
+})
+
+export const decomposeAction = (action: EthereumTransactionAction) => {
+  const { to, from, value } = action
+  if (to && from && value) {
+    return {
+      actionType: EthereumChainActionType.ValueTransfer,
+      args: { ...action },
+    }
+  }
+
+  return null
+}
