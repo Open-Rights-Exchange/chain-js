deaf5eedda085ca0e818cb85056ec2f6326da2ab
basarrcan <basarrcan@gmail.com>
2020-05-21 01:18:04 +0300

    EthChainActionTypes added

--- src/chains/ethereum_1/models/chainActionTypeModels.ts
+++ src/chains/ethereum_1/models/chainActionTypeModels.ts
@@ -1,5 +1,12 @@
 export enum EthereumChainActionType {
-  CategorySomeAction = 'CategorySomeAction',
-  TokenTransfer = 'TokenTransfer',
-  ValueTransfer = 'ValueTransfer',
+  Erc20Burn = 'Erc20Burn',
+  Erc20Transfer = 'Erc20Transfer',
+  Erc20TransferFrom = 'Erc20TransferFrom',
+  Erc20Approve = 'Erc20Approve',
+  Erc20Issue = 'Erc20Issue',
+  Erc20Retire = 'Erc20Retire',
+  Erc677TransferAndCall = 'Erc677TransferAndCall',
+  Erc721Approve = 'Erc721Approve',
+  Erc721Transfer = 'Erc721Transfer',
+  Erc721TransferFrom = 'Erc721TransferFrom',
 }
