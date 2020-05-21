7999dcabaf200b3abbf72e2d0b1374f65155892a
basarrcan <basarrcan@gmail.com>
2020-05-21 04:03:22 +0300

    add templates to ethCompose

--- src/chains/ethereum_1/ethCompose.ts
+++ src/chains/ethereum_1/ethCompose.ts
@@ -4,14 +4,32 @@
 import { ChainActionType } from '../../models'
 import { notSupported } from '../../helpers'
 
-import { composeAction as CategorySomeActionTemplate } from './templates/chainActions/categorySomeAction'
+import { composeAction as ValueTransferTemplate } from './templates/chainActions/value_transfer'
 import { composeAction as TokenTransferTemplate } from './templates/chainActions/token_transfer'
+import { composeAction as ERC20ApproveTemplate } from './templates/chainActions/erc20_approve'
+import { composeAction as ERC20BurnTemplate } from './templates/chainActions/erc20_burn'
+import { composeAction as ERC20IssueTemplate } from './templates/chainActions/erc20_issue'
+import { composeAction as ERC20TransferTemplate } from './templates/chainActions/erc20_transfer'
+import { composeAction as ERC20TransferFromTemplate } from './templates/chainActions/erc20_transferFrom'
+import { composeAction as ERC677TransferAndCallTemplate } from './templates/chainActions/erc677_transferAndCall'
+import { composeAction as ERC721ApproveTemplate } from './templates/chainActions/erc721_approve'
+import { composeAction as ERC721TransferTemplate } from './templates/chainActions/erc721_transfer'
+import { composeAction as ERC721TransferFromTemplate } from './templates/chainActions/erc721_transferFrom'
 import { EthereumTransactionAction, EthereumChainActionType } from './models'
 
 // map a key name to a function that returns an object
 const ComposeAction: { [key: string]: (args: any) => any } = {
-  CategorySomeAction: CategorySomeActionTemplate,
+  ValueTransfer: ValueTransferTemplate,
   TokenTransfer: TokenTransferTemplate,
+  ERC20Approve: ERC20ApproveTemplate,
+  ERC20Burn: ERC20BurnTemplate,
+  ERC20Issue: ERC20IssueTemplate,
+  ERC20Transfer: ERC20TransferTemplate,
+  ERC20TransferFrom: ERC20TransferFromTemplate,
+  ERC677TransferAndCall: ERC677TransferAndCallTemplate,
+  ERC721Approve: ERC721ApproveTemplate,
+  ERC721Transfer: ERC721TransferTemplate,
+  ERC721TransferFrom: ERC721TransferFromTemplate,
 }
 
 /** Compose an object for a chain contract action */
