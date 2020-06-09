b320975c30268fe070cdf5e1ac260893fc2d2a39
basarrcan <basarrcan@gmail.com>
2020-06-09 03:36:08 +0300

    composed action mockups file created

---
+++ src/chains/ethereum_1/tests/mockups/composedActions.ts
@@ -0,0 +1,100 @@
+import { erc20Abi } from '../../templates/abis/erc20Abi'
+import { erc721Abi } from '../../templates/abis/erc721Abi'
+import { EthereumTransactionAction } from '../../models'
+import { toEthereumTxData } from '../../helpers'
+
+export const composedERC20ApproveAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc20Abi,
+    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b81', 20],
+    method: 'approve',
+  },
+}
+
+export const composedERC20BurnAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc20Abi,
+    parameters: [20],
+    method: 'burn',
+  },
+}
+
+export const composedERC20IssueAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc20Abi,
+    parameters: [20],
+    method: 'issue',
+  },
+}
+
+export const composedERC20TransferAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc20Abi,
+    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b81', 20],
+    method: 'transfer',
+  },
+}
+
+export const composedERC20TransferFromAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc20Abi,
+    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b80', '0x27105356f6c1ede0e92020e6225e46dc1f496b81', 20],
+    method: 'transferFrom',
+  },
+}
+
+export const composedERC721ApproveAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc721Abi,
+    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b81', 1],
+    method: 'approve',
+  },
+}
+
+export const composedERC721TransferAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc721Abi,
+    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b81', 1],
+    method: 'transfer',
+  },
+}
+
+export const composedERC721TransferFromAction: EthereumTransactionAction = {
+  to: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
+  from: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
+  contract: {
+    abi: erc721Abi,
+    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b80', '0x27105356f6c1ede0e92020e6225e46dc1f496b81', 1],
+    method: 'transferFrom',
+  },
+}
