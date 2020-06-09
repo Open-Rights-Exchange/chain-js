9867472d4256cc264fbd9c4838255ee2f871471c
surabhil <surabhil@usc.edu>
2020-06-09 08:48:01 -0700

    algo chain state

--- src/chains/algorand_1/algoChainState.ts
+++ src/chains/algorand_1/algoChainState.ts
@@ -1,8 +1,14 @@
 import algosdk from 'algosdk'
 import { throwAndLogError, throwNewError } from '../../errors'
-import { ChainEndpoint, ChainInfo } from '../../models'
-import { AlgorandConnectionSettings, AlgorandChainSettings, Algo } from './models/generalModels'
+import { ChainEndpoint, ChainInfo, ConfirmType } from '../../models'
+import {
+  Algo,
+  AlgorandChainSettingsCommunicationSettings,
+  AlgorandChainSettings,
+  AlgorandConnectionSettings,
+} from './models/generalModels'
 import { trimTrailingChars } from '../../helpers'
+import { AlgorandTxResult } from './models/transactionModels'
 
 export class AlgorandChainState {
   private _activeEndpoint: ChainEndpoint
@@ -98,57 +104,12 @@ export class AlgorandChainState {
     return { server: url, token, port }
   }
 
-  // /** Retrieve a specific block from the chain */
-  // public async getBlock(blockNumber: EthereumBlockNumber): Promise<BlockTransactionString> {
-  //   try {
-  //     this.assertIsConnected()
-  //     const block = await this._web3.eth.getBlock(blockNumber)
-  //     return block
-  //   } catch (error) {
-  //     const chainError = mapChainError(error, ChainFunctionCategory.Block)
-  //     throw chainError
-  //   }
-  // }
-
-  // /** Retrieve a the current price of gas from the chain */
-  // public async getGasPrice(): Promise<number> {
-  //   try {
-  //     this.assertIsConnected()
-  //     const gasPrice = parseInt(await this._web3.eth.getGasPrice(), 10)
-  //     return gasPrice
-  //   } catch (error) {
-  //     const chainError = mapChainError(error, ChainFunctionCategory.ChainState)
-  //     throw chainError
-  //   }
-  // }
-
   /** Confirm that we've connected to the chain - throw if not */
   public assertIsConnected(): void {
     if (!this._isConnected) {
       throwNewError('Not connected to chain')
     }
   }
-
-  // /** Get transaction count for an address
-  //  *  Useful to calculate transaction nonce propery */
-  // public async getTransactionCount(
-  //   address: EthereumAddress & string,
-  //   defaultBlock: EthereumBlockNumber,
-  // ): Promise<number> {
-  //   try {
-  //     return this._web3.eth.getTransactionCount(ensureHexPrefix(address), defaultBlock)
-  //   } catch (error) {
-  //     const chainError = mapChainError(error, ChainFunctionCategory.Transaction)
-  //     throw chainError
-  //   }
-  // }
-
-  // /** Check if a block includes a transaction */
-  // public blockHasTransaction = (block: BlockTransactionString, transactionId: string): boolean => {
-  //   const { transactions } = block
-  //   const result = transactions?.includes(transactionId)
-  //   return !!result
-  // }
 
   /** Submits the transaction to the chain and waits only until it gets a transaction id
    * Does not wait for the transaction to be finalized on the chain
@@ -164,12 +125,14 @@ export class AlgorandChainState {
   }
 
   async waitForTransactionConfirmation(transactionId: string) {
-    while (true) {
+    let waitingConfirmation = true
+
+    while (waitingConfirmation) {
       // eslint-disable-next-line no-await-in-loop
       const pendingTransaction = await this._algo.pendingTransactionInformation(transactionId)
       if (pendingTransaction.round != null && pendingTransaction.round > 0) {
         // Got the completed Transaction
-        break
+        waitingConfirmation = false
       }
     }
   }
@@ -178,41 +141,41 @@ export class AlgorandChainState {
   /* if ConfirmType.None, returns the transaction hash without waiting for further tx receipt
   /* if ConfirmType.After001, waits for the transaction to finalize on chain and then returns the tx receipt
   */
-  // async sendTransaction(
-  //   signedTransaction: string,
-  //   waitForConfirm: ConfirmType = ConfirmType.None,
-  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  //   communicationSettings?: AlgorandChainSettingsCommunicationSettings,
-  // ): Promise<AlgorandTxResult> {
-  //   const sendResult: Partial<AlgorandTxResult> = {}
+  async sendTransaction(
+    signedTransaction: string,
+    waitForConfirm: ConfirmType = ConfirmType.None,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    communicationSettings?: AlgorandChainSettingsCommunicationSettings,
+  ): Promise<AlgorandTxResult> {
+    const sendResult: Partial<AlgorandTxResult> = {}
 
-  //   if (waitForConfirm !== ConfirmType.None && waitForConfirm !== ConfirmType.After001) {
-  //     throwNewError('Only ConfirmType.None or .After001 are currently supported for waitForConfirm parameters')
-  //   }
+    if (waitForConfirm !== ConfirmType.None && waitForConfirm !== ConfirmType.After001) {
+      throwNewError('Only ConfirmType.None or .After001 are currently supported for waitForConfirm parameters')
+    }
 
-  //   try {
-  //     // returns transactionHash after submitting transaction does NOT wait for confirmation from chain
-  //     if (waitForConfirm === ConfirmType.None) {
-  //       const transactionId = (await this.sendTransactionWithoutWaitingForConfirm(signedTransaction)) as string
-  //       sendResult.chainResponse = null
-  //       sendResult.transactionId = transactionId
-  //     }
-  //     // returns transactionReceipt after submitting transaction AND waiting for a confirmation
-  //     if (waitForConfirm === ConfirmType.After001) {
-  //       const transactionId = await this._algo.sendRawTransaction(signedTransaction)
-  //       await waitForTransactionConfirmation(transactionId)
-  //       const chainResponse = await this._algo.getTransactionCount()
-  //       sendResult.transactionId = sendResult?.chainResponse?.transactionHash
-  //     }
-  //   } catch (error) {
-  //     const chainError = mapChainError(error, ChainFunctionCategory.Transaction)
-  //     throw chainError
-  //   }
+    try {
+      // returns transactionHash after submitting transaction does NOT wait for confirmation from chain
+      if (waitForConfirm === ConfirmType.None) {
+        const transactionId = (await this.sendTransactionWithoutWaitingForConfirm(signedTransaction)) as string
+        sendResult.chainResponse = null
+        sendResult.transactionId = transactionId
+      }
+      // returns transactionReceipt after submitting transaction AND waiting for a confirmation
+      if (waitForConfirm === ConfirmType.After001) {
+        const transactionId = await this._algo.sendRawTransaction(signedTransaction)
+        await this.waitForTransactionConfirmation(transactionId)
+        sendResult.transactionId = transactionId
+        sendResult.chainResponse = await this._algo.transactionById(transactionId)
+      }
+    } catch (error) {
+      // ALGO TODO: map chain error
+      throw new Error()
+    }
 
-  //   return sendResult as EthereumTxResult
-  // }
+    return sendResult as AlgorandTxResult
+  }
 
-  /** Return instance of Web3js API */
+  /** Return instance of algo API */
   public get algo(): Algo {
     this.assertIsConnected()
     return this._algo
