527a37e6d5337bc17ebe46abc564ec5e07bd68f2
basarrcan <basarrcan@gmail.com>
2020-06-09 18:24:06 +0300

    multi chain examples added

--- src/examples/transaction.ts
+++ src/examples/transaction.ts
@@ -3,23 +3,11 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
-import { BN } from 'ethereumjs-util'
-import { toEosSymbol, toEosEntityName } from '../chains/eos_2/helpers'
-import { erc20Abi } from '../chains/ethereum_1/templates/abis/erc20Abi'
-import { erc721Abi } from '../chains/ethereum_1/templates/abis/erc721Abi'
-import { ChainFactory, ChainType, Chain } from '../index'
-import { ChainActionType, ChainEndpoint, PrivateKey, ConfirmType } from '../models'
-import { ChainEthereumV1 } from '../chains/ethereum_1/ChainEthereumV1'
-import { toEthereumPrivateKey, toWei, toEthUnit } from '../chains/ethereum_1/helpers'
-import {
-  EthereumChainSettings,
-  EthereumChainForkType,
-  EthereumTransactionOptions,
-  EthUnit,
-  EthereumBlockType,
-  EthereumChainActionType,
-  EthereumAddress,
-} from '../chains/ethereum_1/models'
+import { toEosEntityName } from '../chains/eos_2/helpers'
+import { ChainFactory, ChainType } from '../index'
+import { ChainActionType, ChainEndpoint } from '../models'
+import { toEthereumPrivateKey } from '../chains/ethereum_1/helpers'
+import { EthereumChainSettings, EthereumChainForkType } from '../chains/ethereum_1/models'
 
 require('dotenv').config()
 
@@ -51,142 +39,81 @@ const ropsten = new ChainFactory().create(ChainType.EthereumV1, ropstenEndpoints
 
 const kylin = new ChainFactory().create(ChainType.EosV2, kylinEndpoints, chainSettings)
 
-const chain = ropsten
-// const chain = kylin
+const ethComposeValueTransferParams = {
+  toAccountName: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
+  amount: 10,
+}
+const eosComposeValueTransferParams = {
+  fromAccountName: toEosEntityName('oreidfunding'),
+  toAccountName: toEosEntityName('proppropprop'),
+  amount: 10,
+}
+const ethComposeTokenTransferParams = {
+  toAccountName: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
+  amount: 10,
+}
+const eosComposeTokenTransferParams = {
+  fromAccountName: toEosEntityName('oreidfunding'),
+  toAccountName: toEosEntityName('proppropprop'),
+  amount: 10,
+  symbol: 'EOS',
+  permission: toEosEntityName('active'),
+}
 
-const privateKey = env.ROPSTEN_erc20acc_PRIVATE_KEY
-  // const privateKey = env.ORE_TESTNET_APPOREID_PRIVATE_KEY
-;(async () => {
+const platforms = [
+  {
+    chain: kylin,
+    privateKey: env.EOS_KYLIN_OREIDFUNDING_PRIVATE_KEY,
+    composeValueTransferParams: eosComposeValueTransferParams,
+    composeTokenTransferParams: eosComposeTokenTransferParams,
+  },
+  {
+    chain: ropsten,
+    privateKey: toEthereumPrivateKey(env.ROPSTEN_erc20acc_PRIVATE_KEY),
+    composeValueTransferParams: ethComposeValueTransferParams,
+    composeTokenTransferParams: ethComposeTokenTransferParams,
+  },
+]
+
+async function runner(platform: any) {
   try {
+    const { chain, privateKey, composeValueTransferParams, composeTokenTransferParams } = platform
     await chain.connect()
-
-    // EthereumRawTransaction type input for setFromRaw()
-    // Defaults all optional properties, so you can set from raw just with to & value OR data
-    const sampleSetFromRawTrx = {
-      to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
-      value: toWei(10, EthUnit.Milliether),
-      //  data: '0x00',
-      //  gasPrice: '0x00',
-      //  gasLimit: '0x00',
-    }
-
-    const ethComposeValueTransferParams = {
-      toAccountName: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
-      amount: 10,
-    }
-
-    const eosComposeValueTransferParams = {
-      toAccountName: toEosEntityName('oreappid'),
-      amount: 10,
-      symbol: toEosSymbol('EOS'),
-    }
-
-    interface tokenTransferParams {
-      fromAccountName?: EthereumAddress
-      toAccountName?: EthereumAddress
-      tokenAmount?: number
-      contractName?: EthereumAddress
-    }
-    const composeTokenTransferParams: tokenTransferParams = {
-      contractName: '0x04825941Ad80A6a869e85606b29c9D25144E91e6',
-      toAccountName: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
-      tokenAmount: 20, // 0xD38ADf7D0204a6f5b7ddDe509378e43B1447CDb6
-    }
-
-    interface erc20TransferParams {
-      contractAddress: EthereumAddress
-      from?: EthereumAddress
-      to: EthereumAddress
-      value: number
-    }
-    const composeERC20TransferParams: erc20TransferParams = {
-      contractAddress: '0x04825941Ad80A6a869e85606b29c9D25144E91e6',
-      to: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
-      value: 20, // 0xD38ADf7D0204a6f5b7ddDe509378e43B1447CDb6
-    }
-
-    interface erc20IssueParams {
-      contractAddress: EthereumAddress
-      from?: EthereumAddress
-      value: number
-    }
-    const composeERC20IssueParams: erc20IssueParams = {
-      contractAddress: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
-      value: 20,
-    }
-
-    interface erc721TransferFromParams {
-      contractAddress: EthereumAddress
-      from?: EthereumAddress
-      transferFrom: EthereumAddress
-      to: EthereumAddress
-      tokenId: number
-    }
-    const composeERC721TransferFromParams: erc721TransferFromParams = {
-      contractAddress: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
-      transferFrom: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
-      to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
-      tokenId: 1,
-    }
+    console.log(privateKey)
 
     // // ---> Sign and send ethereum transfer with compose Action
-    const transaction = await chain.new.Transaction()
-    transaction.actions = [chain.composeAction(ChainActionType.ValueTransfer, ethComposeValueTransferParams)]
-    console.log(transaction.actions[0])
-    const decomposed = chain.decomposeAction(transaction.actions[0])
-    console.log(decomposed)
-    await transaction.prepareToBeSigned()
-    await transaction.validate()
-    await transaction.sign([toEthereumPrivateKey(privateKey)])
-    console.log('missing signatures: ', transaction.missingSignatures)
-    console.log('send response:', JSON.stringify(await transaction.send()))
-
-    // // ---> Sign and send erc20 transfer Transaction
     // const transaction = await chain.new.Transaction()
-    // await transaction.addAction(sampleTransferTrx)
-    // transaction.actions = [chain.composeAction(ChainActionType.TokenTransfer, composeERC20TransferParams)]
+    // transaction.actions = [chain.composeAction(ChainActionType.ValueTransfer, composeValueTransferParams)]
     // console.log(transaction.actions[0])
     // const decomposed = chain.decomposeAction(transaction.actions[0])
     // console.log(decomposed)
     // await transaction.prepareToBeSigned()
     // await transaction.validate()
-    // await transaction.sign([toEthereumPrivateKey(privateKey)])
+    // await transaction.sign([privateKey])
     // console.log('missing signatures: ', transaction.missingSignatures)
     // console.log('send response:', JSON.stringify(await transaction.send()))
 
-    // ---> Sign and send erc20 issue Transaction
-    // const transaction = await chain.new.Transaction()
-    // // await transaction.addAction(sampleTransferTrx)
-    // transaction.actions = [chain.composeAction(ChainActionType.TokenIssue, composeERC20IssueParams)]
-    // console.log(transaction.actions[0])
-    // const decomposed = chain.decomposeAction(transaction.actions[0])
-    // console.log(decomposed)
-    // await transaction.prepareToBeSigned()
-    // await transaction.validate()
-    // await transaction.sign([toEthereumPrivateKey(privateKey)])
-    // console.log('missing signatures: ', transaction.missingSignatures)
-    // console.log('send response:', JSON.stringify(await transaction.send()))
-
-    // // ---> Sign and send ethereum transfer with setFromRaw()
-    // const transaction = await chain.new.Transaction()
-    // // await transaction.addAction(sampleTransferTrx)
-    // await transaction.setFromRaw(sampleSetFromRawTrx)
-    // await transaction.validate()
-    // await transaction.sign([toEthereumPrivateKey(privateKey)])
-    // console.log('missing signatures: ', transaction.missingSignatures)
-    // console.log('send response:', JSON.stringify(await transaction.send()))
-
-    // ---> Compose & Decompose erc721 transferFrom Transaction
-    // const transaction = await chain.new.Transaction()
-    // transaction.actions = [
-    //   chain.composeAction(EthereumChainActionType.ERC721TransferFrom, composeERC721TransferFromParams),
-    // ]
-    // console.log(transaction.actions[0])
-    // const decomposed = chain.decomposeAction(transaction.actions[0])
-    // console.log(decomposed)
-    // await transaction.prepareToBeSigned()
-    // await transaction.validate()
+    //   // ---> Sign and send erc20 transfer Transaction
+    //   const transaction = await chain.new.Transaction()
+    //   transaction.actions = [chain.composeAction(ChainActionType.TokenTransfer, composeTokenTransferParams)]
+    //   console.log(transaction.actions[0])
+    //   const decomposed = chain.decomposeAction(transaction.actions[0])
+    //   console.log(decomposed)
+    //   await transaction.prepareToBeSigned()
+    //   await transaction.validate()
+    //   await transaction.sign([privateKey])
+    //   console.log('missing signatures: ', transaction.missingSignatures)
+    //   console.log('send response:', JSON.stringify(await transaction.send()))
   } catch (error) {
     console.log(error)
+  }
+}
+;(async () => {
+  try {
+    platforms.forEach(platform => {
+      runner(platform)
+    })
+  } catch (err) {
+    console.log(err)
   }
 })()
