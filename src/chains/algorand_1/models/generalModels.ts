21a69ff3e27b6e45e6e83213581a37501011bb15
surabhil <surabhil@usc.edu>
2020-06-09 08:47:44 -0700

    algo general models

--- src/chains/algorand_1/models/generalModels.ts
+++ src/chains/algorand_1/models/generalModels.ts
@@ -1,5 +1,10 @@
 export type Algo = any
 
+/**
+ * server: chain endpoint
+ * token: api token required to access the chain network
+ * token's format: {'X-API-Key': '...'}
+ */
 export type AlgorandConnectionSettings = {
   server: string
   token: string
@@ -7,5 +12,9 @@ export type AlgorandConnectionSettings = {
 }
 
 /**  ALGO TODO: Currently nothing is needed in algorand chain settings. 
- Once any such parameter is there, change the type from any to an object containing specigic properties */
+ Once any such parameter is there, change the type from any to an object containing specific properties */
 export type AlgorandChainSettings = any
+
+/**  ALGO TODO: Currently nothing is needed in algorand chain communication settings. 
+ Once any such parameter is there, change the type from any to an object containing specific properties */
+export type AlgorandChainSettingsCommunicationSettings = any
