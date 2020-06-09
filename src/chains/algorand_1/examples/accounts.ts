42ee4f7324e08e352d9105af4b668ec46715a657
surabhil <surabhil@usc.edu>
2020-06-09 10:39:40 -0700

    update example

--- src/chains/algorand_1/examples/accounts.ts
+++ src/chains/algorand_1/examples/accounts.ts
@@ -3,9 +3,11 @@ import { ChainEndpoint } from '../../../models'
 
 require('dotenv').config()
 
+const algoPureStakeTestnet = 'https://testnet-algorand.api.purestake.io/ps1'
+
 export const algoTestnetEndpoints: ChainEndpoint[] = [
   {
-    url: new URL('https://testnet-algorand.api.purestake.io/ps1'),
+    url: new URL(algoPureStakeTestnet),
     settings: {
       token: {
         'X-API-Key': '7n0G2itKl885HQQzEfwtn4SSE1b6X3nb6zVnUw99',
@@ -17,4 +19,7 @@ export const algoTestnetEndpoints: ChainEndpoint[] = [
   // Create an Algo chain instance and call a few functions
   const algoTest = new ChainFactory().create(ChainType.AlgorandV1, algoTestnetEndpoints)
   await algoTest.connect()
+  if (algoTest.isConnected) {
+    console.log('Connected to %o', algoPureStakeTestnet)
+  }
 })()
