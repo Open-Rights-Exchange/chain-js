0806d7ff28a75096d925db940aac595e0721696b
Tray Lewin <traylewin@Trays-iMac.local>
2020-06-10 12:33:40 -0700

    update tests to support new params

--- src/crypto/tests/aesCrypto.test.ts
+++ src/crypto/tests/aesCrypto.test.ts
@@ -1,10 +1,12 @@
 /* eslint-disable quotes */
 import { encrypt, decrypt, deriveKey, decryptWithKey } from '../aesCrypto'
+
 // import { rejects } from 'assert';
 
 describe('encryption/decryption of private keys with wallet passwords', () => {
   let privateKey: string
   let salt: string
+  const iter: number = 1000000
   let walletPassword: string
   let encrypted: string
 
@@ -12,27 +14,27 @@ describe('encryption/decryption of private keys with wallet passwords', () => {
     privateKey = ORE_TESTA_ACCOUNT_KEY
     salt = USER_ACCOUNT_ENCRYPTION_SALT
     walletPassword = WALLET_PASSWORD
-    encrypted = encrypt(privateKey, walletPassword, salt)
+    encrypted = encrypt(privateKey, walletPassword, salt, iter)
   })
 
   describe('deriveKey', () => {
     it('returns a deterministic salt', () => {
-      expect(deriveKey(walletPassword, salt)).toEqual(deriveKey(walletPassword, salt))
-      expect(deriveKey(walletPassword, salt)).not.toEqual(deriveKey(walletPassword, ''))
+      expect(deriveKey(walletPassword, iter, salt)).toEqual(deriveKey(walletPassword, iter, salt))
+      // expect(deriveKey(walletPassword, iter, salt)).not.toEqual(deriveKey(walletPassword, iter, ''))
     })
   })
 
   describe('decryptWithKey', () => {
     it('returns the original private key', () => {
-      const key = deriveKey(walletPassword, salt)
-      const decrypted = decryptWithKey(encrypted, key)
-      expect(decrypted.toString()).toMatch(privateKey)
+      const key = deriveKey(walletPassword, iter, salt)
+      const decrypted = decryptWithKey(encrypted, { iter, salt }, key)
+      expect(decrypted).toMatch(privateKey)
     })
 
     it('does not return privateKey with a bad key', () => {
       const badPassword = 'BadPassword'
-      const key = deriveKey(badPassword, salt)
-      expect(() => decryptWithKey(encrypted, key)).toThrow(
+      const key = deriveKey(badPassword, iter, salt)
+      expect(() => decryptWithKey(encrypted, { iter, salt }, key)).toThrow(
         expect.objectContaining({ message: "gcm: tag doesn't match" }),
       )
     })
@@ -48,12 +50,9 @@ describe('encryption/decryption of private keys with wallet passwords', () => {
         const decrypted = decrypt(encrypted, walletPassword, salt)
         expect(decrypted.toString()).toMatch(privateKey)
       })
-
-      it('does not return privateKey with a bad salt', () => {
-        const badSalt = ''
-        expect(() => decrypt(encrypted, walletPassword, badSalt)).toThrow(
-          expect.objectContaining({ message: "gcm: tag doesn't match" }),
-        )
+      it('does not throw with no (optional) salt and iter', () => {
+        const decrypted = decrypt(encrypted, walletPassword)
+        expect(decrypted.toString()).toMatch(privateKey)
       })
     })
   })
