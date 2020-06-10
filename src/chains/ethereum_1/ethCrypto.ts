c06bd04c204331c121b5581279a9e47d1e5a9147
Tray Lewin <traylewin@Trays-iMac.local>
2020-06-10 13:06:20 -0700

    Eth - Support iter, mode, and optional salt params

--- src/chains/ethereum_1/ethCrypto.ts
+++ src/chains/ethereum_1/ethCrypto.ts
@@ -4,11 +4,16 @@ import { bufferToHex, ecsign, ecrecover, publicToAddress } from 'ethereumjs-util
 import * as aesCrypto from '../../crypto/aesCrypto'
 import { toBuffer, notImplemented } from '../../helpers'
 import { throwNewError } from '../../errors'
-import { EthereumAddress, EthereumPrivateKey, EthereumPublicKey, EthereumSignature } from './models'
+import { EncryptionMode, EthereumAddress, EthereumPrivateKey, EthereumPublicKey, EthereumSignature } from './models'
 import { toEthBuffer } from './helpers/generalHelpers'
 // eslint-disable-next-line import/no-cycle
 import { toEthereumPublicKey, toEthereumSignature } from './helpers/cryptoModelHelpers'
 import { EncryptedDataString } from '../../models'
+
+// eslint-disable-next-line prefer-destructuring
+export const defaultIter = aesCrypto.defaultIter
+// eslint-disable-next-line prefer-destructuring
+export const defaultMode = aesCrypto.defaultMode
 
 /** Verifies that the value is a valid, stringified JSON Encrypted object */
 export function isEncryptedDataString(value: string): value is EncryptedDataString {
@@ -20,18 +25,30 @@ export function toEncryptedDataString(value: any): EncryptedDataString {
   return aesCrypto.toEncryptedDataString(value)
 }
 
-/** Decrypts the encrypted value using a password, and salt using AES algorithm and SHA256 hash function
+/** Decrypts the encrypted value using a password, and optional salt using AES algorithm and SHA256 hash function
  * The encrypted value is either a stringified JSON object or a JSON object */
-export function decrypt(encrypted: EncryptedDataString | any, password: string, salt: string): string {
-  return aesCrypto.decrypt(encrypted, password, salt)
+export function decrypt(
+  encrypted: EncryptedDataString | any,
+  password: string,
+  salt?: string,
+  iter: number = defaultIter, // optional
+  mode: EncryptionMode = defaultMode, // optional
+): string {
+  return aesCrypto.decrypt(encrypted, password, salt, iter, mode)
 }
 
-/** Encrypts a string using a password and salt */
-export function encrypt(unencrypted: string, password: string, salt: string): EncryptedDataString {
-  return aesCrypto.encrypt(unencrypted, password, salt)
+/** Encrypts a string using a password and optional salt */
+export function encrypt(
+  unencrypted: string,
+  password: string,
+  salt?: string,
+  iter: number = defaultIter, // optional
+  mode: EncryptionMode = defaultMode, // optional
+): EncryptedDataString {
+  return aesCrypto.encrypt(unencrypted, password, salt, iter, mode)
 }
 
-/** Signs data with ethereum private key */
+/** Signs data with private key */
 export function sign(data: string | Buffer, privateKey: string): EthereumSignature {
   const dataBuffer = toEthBuffer(data)
   const keyBuffer = toBuffer(privateKey, 'hex')
@@ -54,25 +71,39 @@ export function getEthereumAddressFromPublicKey(publicKey: EthereumPublicKey): E
 }
 
 /** Replaces unencrypted privateKey in keys object
- *  Encrypts key using password and salt */
-export function encryptAccountPrivateKeysIfNeeded(keys: any, password: string, salt: string) {
+ *  Encrypts key using password and optional salt */
+function encryptAccountPrivateKeysIfNeeded(
+  keys: any,
+  password: string,
+  salt?: string,
+  iter?: number,
+  mode?: EncryptionMode,
+) {
   const { privateKey, publicKey } = keys
   const encryptedKeys = {
-    privateKey: isEncryptedDataString(privateKey) ? privateKey : encrypt(privateKey, password, salt).toString(),
+    privateKey: isEncryptedDataString(privateKey)
+      ? privateKey
+      : encrypt(privateKey, password, salt, iter, mode).toString(),
     publicKey,
   }
   return encryptedKeys
 }
 
 /** Generates new public and private key pair
- * Encrypts the private key using password and salt
+ * Encrypts the private key using password and optional salt
  */
-export function generateNewAccountKeysAndEncryptPrivateKeys(password: string, salt: string, overrideKeys: any): any {
+export function generateNewAccountKeysAndEncryptPrivateKeys(
+  password: string,
+  overrideKeys: any,
+  salt?: string,
+  iter?: number,
+  mode?: EncryptionMode,
+): any {
   const wallet = Wallet.generate()
   const privateKey: EthereumPrivateKey = wallet.getPrivateKeyString()
   const publicKey: EthereumPublicKey = wallet.getPublicKeyString()
   const keys = { privateKey, publicKey }
-  const encryptedKeys = encryptAccountPrivateKeysIfNeeded(keys, password, salt)
+  const encryptedKeys = encryptAccountPrivateKeysIfNeeded(keys, password, salt, iter, mode)
   return encryptedKeys
 }
 
