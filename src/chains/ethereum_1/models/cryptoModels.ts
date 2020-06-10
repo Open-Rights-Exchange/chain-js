dd27d57c773dd5e0a0aa6702b2d96b9d6d352c09
Tray Lewin <traylewin@Trays-iMac.local>
2020-06-10 12:50:42 -0700

    Eth - add EncryptionMode type

--- src/chains/ethereum_1/models/cryptoModels.ts
+++ src/chains/ethereum_1/models/cryptoModels.ts
@@ -1,4 +1,5 @@
 import { SignatureBrand, PrivateKeyBrand, PublicKeyBrand } from '../../../models'
+import * as aesCrypto from '../../../crypto/aesCrypto'
 
 /** an ethereum transaction signature */
 export interface ECDSASignature {
@@ -6,6 +7,8 @@ export interface ECDSASignature {
   r: Buffer
   s: Buffer
 }
+
+export type EncryptionMode = aesCrypto.EncryptionMode
 
 /** a private key string - formatted correctly for ethereum */
 export type EthereumPrivateKey = string & PrivateKeyBrand
@@ -15,32 +18,3 @@ export type EthereumPublicKey = string & PublicKeyBrand
 
 /** a signature string - formatted correcly for ethereum */
 export type EthereumSignature = ECDSASignature & SignatureBrand
-
-/** Ethereum value units */
-export enum EthUnit {
-  Noether = 'noether',
-  Wei = 'wei',
-  Kwei = 'kwei',
-  Babbage = 'babbage',
-  femtoether = 'femtoether',
-  Mwei = 'mwei',
-  Lovelace = 'lovelace',
-  Picoether = 'picoether',
-  Qwei = 'gwei',
-  Gwei = 'Gwei',
-  Shannon = 'shannon',
-  Nanoether = 'nanoether',
-  Nano = 'nano',
-  Szabo = 'szabo',
-  Microether = 'microether',
-  Micro = 'micro',
-  Finney = 'finney',
-  Milliether = 'milliether',
-  Milli = 'milli',
-  Ether = 'ether',
-  Kether = 'kether',
-  Grand = 'grand',
-  Mether = 'mether',
-  Gether = 'gether',
-  Thether = 'tether',
-}
