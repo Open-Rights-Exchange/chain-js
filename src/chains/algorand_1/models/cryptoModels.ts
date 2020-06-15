6c63aaa0f708d1725d93812e4533908af3dcf08f
surabhil <surabhil@usc.edu>
2020-06-15 16:49:18 -0700

    update algorand crypto models

--- src/chains/algorand_1/models/cryptoModels.ts
+++ src/chains/algorand_1/models/cryptoModels.ts
@@ -1,10 +1,25 @@
 import { PrivateKeyBrand, PublicKeyBrand, SignatureBrand } from '../../../models'
 
-/** a private key string - formatted correctly for ethereum */
+/** an address string - formatted correctly for algorand */
+export type AlgorandAddress = string
+
+/** an account object - in the format returned from algosdk */
+export type AlgorandAccount = {
+  addr: AlgorandAddress
+  sk: AlgorandPrivateKey
+}
+
+/** key pair - in the format returned from algosdk */
+export type AlgorandKeyPair = {
+  publicKey: AlgorandPublicKey
+  privateKey: AlgorandPrivateKey
+}
+
+/** a private key string - formatted correctly for algorand */
 export type AlgorandPrivateKey = string & PrivateKeyBrand
 
-/** a public key string - formatted correctly for ethereum */
+/** a public key string - formatted correctly for algorand */
 export type AlgorandPublicKey = string & PublicKeyBrand
 
-/** a signature string - formatted correcly for ethereum */
+/** a signature string - formatted correcly for algorand */
 export type AlgorandSignature = Uint8Array & SignatureBrand
