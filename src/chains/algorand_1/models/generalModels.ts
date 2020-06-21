9a61e92f66671f181307c090a455242164044be5
surabhil <surabhil@usc.edu>
2020-06-20 17:55:25 -0700

    add algorand create account options

--- src/chains/algorand_1/models/generalModels.ts
+++ src/chains/algorand_1/models/generalModels.ts
@@ -1,3 +1,6 @@
+import { EncryptedDataString } from '../../../models'
+import { AlgorandAddress, AlgorandPublicKey, AlgorandPrivateKey } from './cryptoModels'
+
 export type AlgoClient = any
 
 /**
@@ -18,3 +21,19 @@ export type AlgorandChainSettings = any
 /**  Currently nothing is needed in algorand chain communication settings. 
  Once any such parameter is there, change the type from any to an object containing specific properties */
 export type AlgorandChainSettingsCommunicationSettings = any
+
+export type AlgorandMultiSigOptions = {
+  version: Number
+  threshold: Number
+  addresses: AlgorandAddress[]
+}
+
+export type AlgorandGeneratedKeys = {
+  publicKey: AlgorandPublicKey
+  privateKey: AlgorandPrivateKey | EncryptedDataString
+}
+
+export type AlgorandNewKeysOptions = {
+  password: string
+  multiSigOptions?: AlgorandMultiSigOptions
+}
