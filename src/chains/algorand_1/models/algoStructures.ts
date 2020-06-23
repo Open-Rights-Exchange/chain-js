09f64a7fafd12c0d25c3b4c773da2f49df20bf12
surabhil <surabhil@usc.edu>
2020-06-23 14:42:51 -0700

    add algorand account structure

--- src/chains/algorand_1/models/algoStructures.ts
+++ src/chains/algorand_1/models/algoStructures.ts
@@ -1,5 +1,47 @@
-/** an account object - in the format returned from algosdk */
+import { AlgorandAddress } from './cryptoModels'
+
+/** Block producer participation information stored within the account if the account is eligible to produce blocks */
+export type AlgorandProducerParticipationStruct = {
+  partpkb64: string
+  votefst: number
+  votekd: number
+  votelst: number
+  vrfpkb64: string
+}
+
+/** Algorand assets held by an Algorand account */
+export type AlgorandAssetStruct = {
+  creator?: string
+  amount: number
+  frozen: boolean
+}
+
+/** Algorand asset information stored on the chain */
+export type AlgorandAssetParamsStruct = {
+  assetname: string
+  clawbackaddr: AlgorandAddress
+  creator: AlgorandAddress
+  decimals: number
+  defaultfrozen: boolean
+  freezeaddr: AlgorandAddress
+  managerkey: AlgorandAddress
+  metadatahash?: string
+  reserveaddr: AlgorandAddress
+  total: number
+  unitname: string
+  url: URL
+}
+
+/** Algorand account information stored on the chain */
 export type AlgorandAccountStruct = {
-  addr: Uint8Array
-  sk: Uint8Array
+  address: AlgorandAddress
+  amount?: number
+  Amountwithoutpendingrewards?: number
+  assets?: Map<string, AlgorandAssetStruct>
+  participation?: AlgorandProducerParticipationStruct
+  pendingrewards?: number
+  rewards?: number
+  round?: number
+  status?: string
+  thisassettotal?: Map<string, AlgorandAssetParamsStruct>
 }
