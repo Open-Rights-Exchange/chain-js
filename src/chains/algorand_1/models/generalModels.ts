90560bf8ca260cc8cdf0861fcacec2c9301654a3
surabhil <surabhil@usc.edu>
2020-06-23 08:30:44 -0700

    multisig account types

--- src/chains/algorand_1/models/generalModels.ts
+++ src/chains/algorand_1/models/generalModels.ts
@@ -22,17 +22,23 @@ export type AlgorandChainSettings = any
  Once any such parameter is there, change the type from any to an object containing specific properties */
 export type AlgorandChainSettingsCommunicationSettings = any
 
+/**  Multisig options required to create a multisignature account for Algorand */
 export type AlgorandMultiSigOptions = {
   version: Number
   threshold: Number
-  addresses: AlgorandAddress[]
+  accounts: AlgorandAddress[]
 }
 
+/**  Algorand multisig account is similar to a native algorand address */
+export type AlgorandMutliSigAccount = AlgorandAddress
+
+/**  Algorand generated keys  after an account is created */
 export type AlgorandGeneratedKeys = {
   publicKey: AlgorandPublicKey
-  privateKey: AlgorandPrivateKey | EncryptedDataString
+  privateKey?: AlgorandPrivateKey | EncryptedDataString
 }
 
+/**  Algorand new keys options including password and optional multisig parameters */
 export type AlgorandNewKeysOptions = {
   password: string
   multiSigOptions?: AlgorandMultiSigOptions
