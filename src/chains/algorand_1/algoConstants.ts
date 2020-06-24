ea06dadbb7e457cd9b89aeb869f294b99faf9822
surabhil <surabhil@usc.edu>
2020-06-24 11:43:37 -0700

    enum algo unit

--- src/chains/algorand_1/algoConstants.ts
+++ src/chains/algorand_1/algoConstants.ts
@@ -1,4 +1,10 @@
+import { AlgorandUnit } from './models/generalModels'
+
 export const DEFAULT_TIMEOUT_FOR_TRX_CONFIRM = 500
+export const DEFAULT_ALGO_SYMBOL = AlgorandUnit.Microalgo
+
 export const ALGORAND_CHECKSUM_BYTE_LENGTH = 4
 export const ALGORAND_ADDRESS_LENGTH = 58
 export const ALGORAND_ADDRESS_BYTE_LENGTH = 36
+
+export const ALGORAND_POST_CONTENT_TYPE = { 'content-type': 'application/x-binary' }
