591a896bc3cfbfec47514441546ba53b85b87a36
surabhil <surabhil@usc.edu>
2020-06-24 16:32:57 -0700

    update example

--- src/chains/algorand_1/examples/misc.ts
+++ src/chains/algorand_1/examples/misc.ts
@@ -26,27 +26,6 @@ export const algoTestnetEndpoints: ChainEndpoint[] = [
   },
 ]
 
-export const CreateAccountOptions = {
-  newKeysOptions: {
-    password: '2233',
-  },
-}
-
-export const multiSigOptions: AlgorandMultiSigOptions = {
-  version: 1,
-  threshold: 2,
-  accounts: [
-    'LE6IM6NPZ7DPF3LTVQT62ARING2VSXO7KOYFHCUCNXKSQLVW4I3AKXUMPI',
-    'S4N2Q4H3ZDZHR7OL6C7FLXI76K7W5XTPFNOXHOSUPL7GPMXU4PT4XCHXLE',
-    'O24FOKUAML2OB3KLQXWIZ2S4VFTEMO6E2PZFYBDT6HX22C5O7DCRHUIUWU',
-  ],
-}
-
-export const CreateMultiSigAccountOptions = {
-  ...CreateAccountOptions,
-  multiSigOptions,
-}
-
 async function run() {
   /** Create Algorand chain instance */
   const algoTest = new ChainFactory().create(ChainType.AlgorandV1, algoTestnetEndpoints)
