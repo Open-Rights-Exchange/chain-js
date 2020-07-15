3503bb1070d6cab9883febbf9a83e0f85f9ecdc4
surabhil <surabhil@usc.edu>
2020-07-15 16:31:02 -0700

    aplhabetize crypto model helpers

--- src/chains/algorand_1/helpers/cryptoModelHelpers.ts
+++ src/chains/algorand_1/helpers/cryptoModelHelpers.ts
@@ -1,8 +1,36 @@
+import scrypt from 'scrypt-async'
 import { decodeUTF8 } from 'tweetnacl-util'
 import * as sha512 from 'js-sha512'
 import { isNullOrEmpty } from '../../../helpers'
 import { AlgorandPublicKey, AlgorandSignature, AlgorandPrivateKey } from '../models'
 import * as ed25519Crypto from '../../../crypto/ed25519Crypto'
+import { ALGORAND_PASSWORD_ENCRYPTION_CONSTANTS } from '../algoConstants'
+
+/** Converts a password string using salt to a key(32 byte array)
+ * Derives a key from password and salt and calls callback with the derived key as the only argument.
+ */
+export function calculatePasswordByteArray(password: string, salt: string): Uint8Array {
+  let passwordArray
+  scrypt(password, salt, ALGORAND_PASSWORD_ENCRYPTION_CONSTANTS, function(derivedKey: any) {
+    passwordArray = derivedKey
+  })
+  return passwordArray
+}
+
+/**
+ * ConcatArrays takes two array and returns a joint Uint8 array of both
+ */
+export function concatArrays(a: any, b: any): Uint8Array {
+  const c = new Uint8Array(a.length + b.length)
+  c.set(a)
+  c.set(b, a.length)
+  return c
+}
+
+/** Return sha512 hash of an array */
+export function genericHash(arr: Uint8Array) {
+  return sha512.sha512_256.array(arr)
+}
 
 export function isValidAlgorandPublicKey(value: string | AlgorandPublicKey): value is AlgorandPublicKey {
   if (!value) return false
@@ -11,6 +39,14 @@ export function isValidAlgorandPublicKey(value: string | AlgorandPublicKey): val
 
 export function isValidAlgorandPrivateKey(value: string): value is AlgorandPrivateKey {
   return ed25519Crypto.isValidPrivateKey(ed25519Crypto.hexStringToByteArray(value))
+}
+
+// ALGO TODO: add validation rule for signature
+export function isValidAlgorandSignature(signature: string): boolean {
+  if (!isNullOrEmpty(signature)) {
+    return true
+  }
+  return false
 }
 
 /** Accepts hex string checks if a valid algorand private key
@@ -33,30 +69,14 @@ export function toAlgorandPublicKey(value: string): AlgorandPublicKey {
   throw new Error(`Not a valid algorand public key:${value}.`)
 }
 
+export function toAlgorandSignature(value: string): AlgorandSignature {
+  if (isValidAlgorandSignature(value)) {
+    return value as AlgorandSignature
+  }
+  throw new Error(`Not a valid algorand signature:${value}.`)
+}
+
 /** Converts a string to an Uint8 array */
 export function toUint8Array(encodedString: string) {
   return decodeUTF8(encodedString)
-}
-
-/** Return sha512 hash of an array */
-export function genericHash(arr: Uint8Array) {
-  return sha512.sha512_256.array(arr)
-}
-
-/**
- * ConcatArrays takes two array and returns a joint Uint8 array of both
- */
-export function concatArrays(a: any, b: any): Uint8Array {
-  const c = new Uint8Array(a.length + b.length)
-  c.set(a)
-  c.set(b, a.length)
-  return c
-}
-
-// ALGO TODO: add validation rule for signature
-export function isValidAlgorandSignature(signature: AlgorandSignature): boolean {
-  if (!isNullOrEmpty(signature)) {
-    return true
-  }
-  return false
 }
