18f17bee67463655ddf65384b2bf8ecc2af7f06d
surabhil <surabhil@usc.edu>
2020-07-20 15:07:59 -0700

    make compose action async and pass chainstate

--- src/chains/algorand_1/algoCompose.ts
+++ src/chains/algorand_1/algoCompose.ts
@@ -2,6 +2,8 @@ import { ChainActionType } from '../../models'
 import { notSupported } from '../../helpers'
 import { composeAction as ValueTransferTemplate } from './templates/chainActions/standard/value_transfer'
 import { AlgorandChainActionType, AlgorandTransactionAction } from './models'
+import { AlgorandChainState } from './algoChainState'
+import { ALGORAND_TRX_COMFIRMATION_ROUNDS } from './algoConstants'
 
 // map a key name to a function that returns an object
 const ComposeAction: { [key: string]: (args: any) => any } = {
@@ -10,13 +12,28 @@ const ComposeAction: { [key: string]: (args: any) => any } = {
 }
 
 /** Compose an object for a chain contract action */
-export function composeAction(
+export async function composeAction(
+  chainState: AlgorandChainState,
   chainActionType: ChainActionType | AlgorandChainActionType,
   args: any,
-): AlgorandTransactionAction {
+): Promise<AlgorandTransactionAction> {
   const composerFunction = ComposeAction[chainActionType as string]
   if (!composerFunction) {
     notSupported()
   }
-  return composerFunction(args)
+  const suggestedParams = await chainState.algoClient.getTransactionParams()
+  const { genesishashb64: genesisHash, minFee, lastRound: lastRoundFromChain } = suggestedParams
+  const { fee: fixedFee, flatFee = false, firstRound: inputFirstRound, lastRound: inputLastRound } = args
+  const fee = flatFee ? fixedFee : minFee
+  const firstRound = inputFirstRound || lastRoundFromChain
+  const lastRound = inputLastRound || firstRound + ALGORAND_TRX_COMFIRMATION_ROUNDS
+  const composeArgs = {
+    ...suggestedParams,
+    ...args,
+    fee,
+    genesisHash,
+    firstRound,
+    lastRound,
+  }
+  return composerFunction(composeArgs)
 }
