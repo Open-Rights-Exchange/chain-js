0207d482a8ed72cfeac130a98841caf1ff46e8d8
Tray Lewin <tray.lewin@live.com>
2020-07-23 17:13:17 -0700

    Algo - add support for actionHelper to compose

--- src/chains/algorand_1/algoCompose.ts
+++ src/chains/algorand_1/algoCompose.ts
@@ -2,12 +2,18 @@ import { ChainActionType } from '../../models'
 import { notSupported } from '../../helpers'
 import { composeAction as ValueTransferTemplate } from './templates/chainActions/standard/value_transfer'
 import { composeAction as AssetTransferTemplate } from './templates/chainActions/chainSpecific/asset_transfer'
-import { AlgorandChainActionType, AlgorandTransactionAction } from './models'
+import {
+  AlgorandChainActionType,
+  AlgorandChainTransactionParamsStruct,
+  AlgorandTxAction,
+  AlgorandTxActionRaw,
+  AlgorandTxHeaderParams,
+} from './models'
 import { AlgorandChainState } from './algoChainState'
-import { ALGORAND_TRX_COMFIRMATION_ROUNDS } from './algoConstants'
+import { AlgorandActionHelper } from './algoAction'
 
 // map a key name to a function that returns an object
-const ComposeAction: { [key: string]: (args: any) => any } = {
+const ComposeAction: { [key: string]: (args: any, suggestedParams: AlgorandTxHeaderParams) => any } = {
   // Standard actions
   ValueTransfer: ValueTransferTemplate,
   // Algorand actions
@@ -19,24 +25,21 @@ export async function composeAction(
   chainState: AlgorandChainState,
   chainActionType: ChainActionType | AlgorandChainActionType,
   args: any,
-): Promise<AlgorandTransactionAction> {
+): Promise<AlgorandTxAction> {
   const composerFunction = ComposeAction[chainActionType as string]
   if (!composerFunction) {
     notSupported()
   }
-  const suggestedParams = await chainState.algoClient.getTransactionParams()
-  const { genesishashb64: genesisHash, minFee, lastRound: lastRoundFromChain } = suggestedParams
-  const { fee: fixedFee, flatFee = false, firstRound: inputFirstRound, lastRound: inputLastRound } = args
-  const fee = flatFee ? fixedFee : minFee
-  const firstRound = inputFirstRound || lastRoundFromChain
-  const lastRound = inputLastRound || firstRound + ALGORAND_TRX_COMFIRMATION_ROUNDS
-  const composeArgs = {
-    ...suggestedParams,
-    ...args,
-    fee,
-    genesisHash,
-    firstRound,
-    lastRound,
+
+  let actionHelper = new AlgorandActionHelper(args)
+  const chainTxHeaderParams: AlgorandChainTransactionParamsStruct = await chainState.algoClient.getTransactionParams()
+  actionHelper.applyCurrentTxHeaderParamsWhereNeeded(chainTxHeaderParams)
+  // seperate-out the action param values (required by compose functions) from the suggestedParams (headers)
+  const rawAction: AlgorandTxActionRaw = composerFunction(actionHelper.paramsOnly, actionHelper.transactionHeaderParams)
+  // use AlgorandActionHelper to drop empty fields
+  actionHelper = new AlgorandActionHelper(rawAction)
+  return {
+    ...actionHelper.transactionHeaderParams,
+    ...actionHelper.paramsOnly,
   }
-  return composerFunction(composeArgs)
 }
