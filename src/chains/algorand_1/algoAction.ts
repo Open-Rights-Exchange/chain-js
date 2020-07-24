fa1d5a7c6079b6deef8b5e451e5abfb5be587bc1
Tray Lewin <tray.lewin@live.com>
2020-07-24 12:39:59 -0700

    Algo - handle action formatted for sdk

--- src/chains/algorand_1/algoAction.ts
+++ src/chains/algorand_1/algoAction.ts
@@ -1,10 +1,10 @@
 import * as algosdk from 'algosdk'
-import { isNullOrEmpty, toBuffer, bufferToString } from '../../helpers'
+import { isNullOrEmpty, toBuffer, bufferToString, isAUint8Array, isAString } from '../../helpers'
 import { throwNewError } from '../../errors'
-import { AlgorandTxAction, AlgorandTxActionRaw } from './models/transactionModels'
+import { AlgorandTxAction, AlgorandTxActionRaw, AlgorandTxActionSdkEncoded } from './models/transactionModels'
 import { AlgorandTxHeaderParams, AlgorandChainTransactionParamsStruct } from './models'
 import { ALGORAND_TRX_COMFIRMATION_ROUNDS, ALGORAND_EMPTY_CONTRACT_NAME } from './algoConstants'
-import { isAlgorandTxActionRaw, toAlgorandAddressFromRaw, toRawAddressFromAlgoAddr } from './helpers'
+import { toAlgorandAddressFromRaw, toRawAddressFromAlgoAddr } from './helpers'
 
 /** Helper class to ensure transaction actions properties are set correctly
  * Algorand supports these actions:
@@ -28,14 +28,16 @@ export class AlgorandActionHelper {
   }
 
   /** applies rules for input params, converts to raw values if needed */
-  private validateAndApplyParams(action: AlgorandTxAction | AlgorandTxActionRaw) {
+  private validateAndApplyParams(action: AlgorandTxAction | AlgorandTxActionRaw | AlgorandTxActionSdkEncoded) {
     if (isNullOrEmpty(action)) {
       throwNewError('Missing action')
     }
     if (isNullOrEmpty(action.from)) {
       throwNewError('Action requires a from value')
     }
-    if (isAlgorandTxActionRaw(action)) {
+    if (this.isAlgorandTxActionEncodedForSdk(action)) {
+      this._rawAction = this.actionEncodedForSdkToRaw(action as AlgorandTxActionSdkEncoded)
+    } else if (this.isAlgorandTxActionRaw(action)) {
       this._rawAction = action as AlgorandTxActionRaw
     } else {
       this._rawAction = this.actionToRaw(action as AlgorandTxAction)
@@ -62,27 +64,30 @@ export class AlgorandActionHelper {
       assetClawback: toAlgorandAddressFromRaw(this.raw.assetClawback),
       freezeAccount: toAlgorandAddressFromRaw(this.raw.freezeAccount),
       assetRevocationTarget: toAlgorandAddressFromRaw(this.raw.assetRevocationTarget),
-      note: algosdk.decodeObj(this.raw.note),
-      tag: bufferToString(this.raw.tag),
+      note: this.raw.note ? algosdk.decodeObj(this.raw.note) : undefined,
+      tag: this.raw.tag ? bufferToString(this.raw.tag) : undefined,
     }
     this.deleteEmptyFields(returnVal)
     return returnVal
   }
 
   /** Action properties formatted to send to algosdk - includes hexstring addresses and UInt8Array note */
-  public get actionForChain(): AlgorandTxAction {
-    const returnVal = this.action
-    // encode for transaction siging
-    if (this.action.note) {
-      returnVal.note = algosdk.encodeObj(this.action.note)
+  public get actionEncodedForSdk(): AlgorandTxActionSdkEncoded {
+    let convertedParams: AlgorandTxActionSdkEncoded = {}
+    const { note, tag, ...otherParams } = this.action
+    // encode fields as required by SDK
+    convertedParams = this.encodeFieldsForSdk(convertedParams)
+    return {
+      ...otherParams,
+      ...convertedParams,
     }
-    return returnVal
   }
 
   /** Action properties with all UInt8Array fields converted to hex strings */
   private actionToRaw(action: AlgorandTxAction) {
+    const rawSdkEncoded = this.encodeFieldsForSdk(action)
     const raw: AlgorandTxActionRaw = {
-      ...action,
+      ...rawSdkEncoded,
       to: toRawAddressFromAlgoAddr(action.to) || undefined,
       from: toRawAddressFromAlgoAddr(action.from) || undefined,
       closeRemainderTo: toRawAddressFromAlgoAddr(action.closeRemainderTo) || undefined,
@@ -92,9 +97,18 @@ export class AlgorandActionHelper {
       assetClawback: toRawAddressFromAlgoAddr(action.assetClawback) || undefined,
       freezeAccount: toRawAddressFromAlgoAddr(action.freezeAccount) || undefined,
       assetRevocationTarget: toRawAddressFromAlgoAddr(action.assetRevocationTarget) || undefined,
-      note: algosdk.encodeObj(action.note),
-      tag: toBuffer(action.tag),
     }
+    // also encode note, tag, etc.
+    return raw
+  }
+
+  /** Convert Action encoded for algo sdk to raw - */
+  private actionEncodedForSdkToRaw(action: AlgorandTxActionSdkEncoded) {
+    const { note, tag, ...otherParams }: { note?: Uint8Array; tag?: Buffer; otherParams?: any } = action
+    const raw: AlgorandTxActionRaw = this.actionToRaw(otherParams as AlgorandTxAction) as AlgorandTxActionRaw
+    // these fields are already encoded as needed for raw - so we dont want them encoded again
+    raw.note = note
+    raw.tag = tag
     return raw
   }
 
@@ -131,6 +145,22 @@ export class AlgorandActionHelper {
     return actionParams
   }
 
+  /** Action properties - excludes 'suggestedParams' fields (e.g. genesisID)
+   *  Selected fields encoded (e.g. note) as required by algo sdk */
+  public get paramsOnlySdkEncoded() {
+    const actionParams = this.paramsOnly
+    return this.encodeFieldsForSdk(actionParams)
+  }
+
+  /** Encode selected fields required by algo sdk */
+  private encodeFieldsForSdk(paramsIn: any) {
+    const { note, tag } = paramsIn
+    const params: any = paramsIn
+    if (note && !isAUint8Array(note)) params.note = algosdk.encodeObj(note)
+    if (tag && !Buffer.isBuffer(note)) params.tag = toBuffer(tag)
+    return params
+  }
+
   /** Adds the latest transaction header fields (firstRound, etc.) from chain
    *  Applies any that are not already provided in the action */
   applyCurrentTxHeaderParamsWhereNeeded(chainTxParams: AlgorandChainTransactionParamsStruct) {
@@ -148,5 +178,24 @@ export class AlgorandActionHelper {
     const params = paramsIn
     Object.keys(params).forEach(key => (isNullOrEmpty(params[key]) ? delete params[key] : {}))
     if (params.lease && (params.lease as Uint8Array).length === 0) delete params.lease
+  }
+
+  /** whether action is the native chain 'raw' format */
+  isAlgorandTxAction(action: AlgorandTxAction | AlgorandTxActionRaw): boolean {
+    return isAString(action.from)
+  }
+
+  /** whether action is the native chain 'raw' format */
+  isAlgorandTxActionRaw(action: AlgorandTxAction | AlgorandTxActionRaw | AlgorandTxActionSdkEncoded): boolean {
+    const rawAction = action as AlgorandTxActionRaw
+    const hasPublicKey = rawAction.from?.publicKey
+    return hasPublicKey && isAUint8Array(rawAction.from?.publicKey)
+  }
+
+  /** whether action is encoded for the algo sdk (from is string and note is UInt8Array) */
+  isAlgorandTxActionEncodedForSdk(
+    action: AlgorandTxAction | AlgorandTxActionRaw | AlgorandTxActionSdkEncoded,
+  ): boolean {
+    return isAString(action.from) && (isAUint8Array(action.note) || Buffer.isBuffer(action.tag))
   }
 }
