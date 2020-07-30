8a11f6de18cf22e4ce76ebfdd672bb725a2b03fb
Tray Lewin <tray.lewin@live.com>
2020-07-30 12:28:05 -0700

    Algo - add getPublicKeyForAddress helper

--- src/chains/algorand_1/helpers/generalModelHelpers.ts
+++ src/chains/algorand_1/helpers/generalModelHelpers.ts
@@ -1,7 +1,7 @@
 import { isValidAddress } from 'algosdk'
 import * as base32 from 'hi-base32'
 import { isNullOrEmpty, byteArrayToHexString } from '../../../helpers'
-import { AlgorandAddress, AlgorandSymbol, AlgorandAddressStruct } from '../models'
+import { AlgorandAddress, AlgorandAddressStruct, AlgorandPublicKey, AlgorandSymbol } from '../models'
 import { toAddressFromPublicKey, toAlgorandPublicKey } from './cryptoModelHelpers'
 import { ALGORAND_ADDRESS_BYTES_ONLY_LENGTH, ALGORAND_CHECKSUM_BYTE_LENGTH, PUBLIC_KEY_LENGTH } from '../algoConstants'
 
@@ -53,4 +53,10 @@ export function toRawAddressFromAlgoAddr(address: AlgorandAddress): AlgorandAddr
     publicKey,
     checksum,
   }
+}
+
+/** Returns publicKey for associated address */
+export function getPublicKeyForAddress(address: AlgorandAddress): AlgorandPublicKey {
+  const rawAddress = toRawAddressFromAlgoAddr(address)
+  return toAlgorandPublicKey(byteArrayToHexString(rawAddress.publicKey))
 }
