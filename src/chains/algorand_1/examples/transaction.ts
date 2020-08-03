1c70809c4073ee4895c93dfceb3df59aea6e82bb
basarrcan <basarrcan@gmail.com>
2020-08-03 19:24:21 +0300

    ALGO - transaction assets example seperated

--- src/chains/algorand_1/examples/transaction.ts
+++ src/chains/algorand_1/examples/transaction.ts
@@ -6,19 +6,7 @@
 
 import { ChainFactory, ChainType } from '../../../index'
 import { ChainEndpoint, ChainActionType } from '../../../models'
-import {
-  AlgorandAddress,
-  AlgorandPrivateKey,
-  AlgorandUnit,
-  AlgorandValue,
-  AlgorandActionAssetConfigParams,
-  AlgorandChainActionType,
-  AlgorandKeyRegistrationParams,
-  AlgorandActionAssetCreateParams,
-  AlgorandActionAssetDestroyParams,
-  AlgorandActionAssetFreezeParams,
-  AlgorandActionAssetTransferParams,
-} from '../models'
+import { AlgorandAddress, AlgorandUnit, AlgorandValue } from '../models'
 import { toAlgorandPrivateKey } from '../helpers'
 
 require('dotenv').config()
@@ -54,65 +42,6 @@ const composeValueTransferParams: valueTransferParams = {
   symbol: AlgorandUnit.Microalgo,
   memo: 'Hello World',
 }
-
-const composeAssetConfigParams: AlgorandActionAssetConfigParams = {
-  from: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  note: 'config',
-  assetIndex: 11137706,
-  assetManager: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetReserve: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetFreeze: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetClawback: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  strictEmptyAddressChecking: false,
-}
-
-const composeAssetCreateParams: AlgorandActionAssetCreateParams = {
-  fromAccountName: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  note: 'create',
-  totalIssuance: 1000,
-  assetDefaultFrozen: false,
-  assetDecimals: 0,
-  assetReserve: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetFreeze: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetClawback: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetManager: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  assetUnitName: 'exp',
-  assetName: 'examplecoin',
-  assetURL: '',
-  assetMetadataHash: '',
-}
-
-const composeAssetDestroyParams: AlgorandActionAssetDestroyParams = {
-  from: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  note: 'destroy',
-  assetIndex: 11137706,
-}
-
-const composeAssetFreezeParams: AlgorandActionAssetFreezeParams = {
-  from: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  note: 'freeze',
-  assetIndex: 11137706,
-  freezeTarget: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  freezeState: true,
-}
-
-const composeAssetTransferParams: AlgorandActionAssetTransferParams = {
-  from: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  to: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  amount: 5,
-  note: 'example',
-  assetIndex: 11137706,
-}
-
-const composeKeyRegistrationParams: AlgorandKeyRegistrationParams = {
-  from: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  note: 'note',
-  voteKey: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  selectionKey: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
-  voteFirst: 6000000,
-  voteLast: 9000000,
-  voteKeyDilution: 1739,
-}
 ;(async () => {
   /** Create Algorand chain instance */
   const algoTest = new ChainFactory().create(ChainType.AlgorandV1, algoTestnetEndpoints)
@@ -121,16 +50,10 @@ const composeKeyRegistrationParams: AlgorandKeyRegistrationParams = {
     console.log('Connected to %o', algoPureStakeTestnet)
   }
 
-  /** Compose and send transaction V */
+  /** Compose and send transaction */
   const transaction = await algoTest.new.Transaction()
   composeValueTransferParams.fromAccountName = 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ'
   const action = await algoTest.composeAction(ChainActionType.ValueTransfer, composeValueTransferParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetCreate, composeAssetCreateParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetConfig, composeAssetConfigParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetDestroy, composeAssetDestroyParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetFreeze, composeAssetFreezeParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetTransfer, composeAssetTransferParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.KeyRegistration, composeKeyRegistrationParams)
   transaction.actions = [action]
   console.log('transaction actions: ', transaction.actions[0])
   const decomposed = algoTest.decomposeAction(transaction.actions[0])
