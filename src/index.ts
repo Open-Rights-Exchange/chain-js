f8b31dd562f18f4f164ccc2bfecab785cdcba45f
Tray Lewin <tray.lewin@live.com>
2020-08-05 16:47:20 -0700

    Export Algorand chain and models

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { ChainFactory } from './chainFactory'
 import { ChainError } from './errors'
 import { Account, Chain, CreateAccount, Transaction } from './interfaces'
+import * as AlgorandV1 from './chains/algorand_1'
 import * as EosV2 from './chains/eos_2'
+import * as EthereumV1 from './chains/ethereum_1'
 import { ChainEosV2 } from './chains/eos_2/ChainEosV2'
 import { ChainEthereumV1 } from './chains/ethereum_1/ChainEthereumV1'
 import * as Crypto from './crypto'
-import * as Ethereum10 from './chains/ethereum_1'
 import * as Models from './models'
+import * as ModelsAlgorand from './chains/algorand_1/models'
 import * as ModelsEos from './chains/eos_2/models'
 import * as ModelsEthereum from './chains/ethereum_1/models'
 
@@ -22,9 +24,11 @@ export {
   ChainType,
   CreateAccount,
   Crypto,
+  AlgorandV1,
   EosV2,
-  Ethereum10,
+  EthereumV1,
   Models,
+  ModelsAlgorand,
   ModelsEos,
   ModelsEthereum,
   Transaction,
