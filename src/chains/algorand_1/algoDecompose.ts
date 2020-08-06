02522048c357e178b0d2f1ca5d07c702ab480502
Tray Lewin <tray.lewin@live.com>
2020-08-06 08:03:55 -0700

    Algo - register payment decompose

--- src/chains/algorand_1/algoDecompose.ts
+++ src/chains/algorand_1/algoDecompose.ts
@@ -8,6 +8,7 @@ import { decomposeAction as AssetDestroyTemplate } from './templates/chainAction
 import { decomposeAction as AssetFreezeTemplate } from './templates/chainActions/chainSpecific/asset_freeze'
 import { decomposeAction as AssetTransferTemplate } from './templates/chainActions/chainSpecific/asset_transfer'
 import { decomposeAction as KeyRegistrationTemplate } from './templates/chainActions/chainSpecific/key_registration'
+import { decomposeAction as PaymentTemplate } from './templates/chainActions/chainSpecific/payment'
 
 // map a key name to a function that returns an object
 const DecomposeAction: { [key: string]: (args: any) => any } = {
@@ -20,6 +21,7 @@ const DecomposeAction: { [key: string]: (args: any) => any } = {
   AssetFreeze: AssetFreezeTemplate,
   AssetTransfer: AssetTransferTemplate,
   KeyRegistration: KeyRegistrationTemplate,
+  Payment: PaymentTemplate,
 }
 
 /** Decompose a transaction action to determine its standard action type (if any) and retrieve its data */
