a435c4858c99abc07d47e277765617dedbdcb8c0
Tray Lewin <tray.lewin@live.com>
2020-08-06 08:03:37 -0700

    Export Helpers from chainjs

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ import * as Models from './models'
 import * as ModelsAlgorand from './chains/algorand_1/models'
 import * as ModelsEos from './chains/eos_2/models'
 import * as ModelsEthereum from './chains/ethereum_1/models'
+import * as HelpersAlgorand from './chains/algorand_1/helpers'
+import * as HelpersEos from './chains/eos_2/helpers'
+import * as HelpersEthereum from './chains/ethereum_1/helpers'
 
 const { ChainType } = Models
 
@@ -27,6 +30,9 @@ export {
   AlgorandV1,
   EosV2,
   EthereumV1,
+  HelpersAlgorand,
+  HelpersEos,
+  HelpersEthereum,
   Models,
   ModelsAlgorand,
   ModelsEos,
