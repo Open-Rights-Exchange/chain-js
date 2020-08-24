ef8081edec4c2bb3e52c7724a279883a3817389f
basarrcan <basarrcan@gmail.com>
2020-08-24 22:22:40 +0300

    ALGO - seperate Rekeying example

--- src/chains/algorand_1/examples/accountRekey.ts
+++ src/chains/algorand_1/examples/accountRekey.ts
@@ -28,36 +28,26 @@ const algoBetanetEndpoints = [{
   url: new URL('https://betanet-algorand.api.purestake.io/ps1'),
   options: { headers: [ { 'X-API-Key': algoApiKey } ] }, 
 }]
-
-const composeTokenTransferParams: Partial<AlgorandActionAssetTransferParams> = {
-  from: 'VBS2IRDUN2E7FJGYEKQXUAQX3XWL6UNBJZZJHB7CJDMWHUKXAGSHU5NXNQ',
+// As of Aug 2020 - Rekey feature only available on beta test net - use beta endpoints to run.
+// requires transaction setup with betanet endpoints and accounts.
+// example is only repeatable by each time switching between composeAlgoReKeyParamsA and B, providing the necessary private key:
+// paramsA -> ALGOBETANET_rekeyaccount_PRIVATE_KEY_A,
+// paramsB -> ALGOBETANET_rekeyaccount_PRIVATE_KEY_B
+// next time transaction will require signature of the new address(reKeyTo)
+const composeAlgoReKeyParamsA: Partial<AlgorandActionPaymentParams> = {
+  from: env.ALGOBETANET_rekeyaccount_A,
   to: 'TF6PJW7VSEKD5AXYMUXF5YGMPDUWBJQRHH4PYJISFPXAMI27PGYHKLALDY',
   note: 'transfer memo',
   amount: 1,
-  assetIndex: 10820019,
+  reKeyTo: env.ALGOBETANET_rekeyaccount_B,
 }
 
-const composeAlgoPaymentParams: Partial<AlgorandActionPaymentParams> = {
-  from: 'WCZV75K44NWDFWTYJCCOJ6NZC2TYAODLIC7GFMMXRKY2JNPW3LTB4IUL3U',
+const composeAlgoReKeyParamsB: Partial<AlgorandActionPaymentParams> = {
+  from: env.ALGOBETANET_rekeyaccount_A,
   to: 'TF6PJW7VSEKD5AXYMUXF5YGMPDUWBJQRHH4PYJISFPXAMI27PGYHKLALDY',
   note: 'transfer memo',
   amount: 1,
-}
-
-// raw transaction ('blob' returned from the Algo SDK transaction sign function)
-const payRawTransaction: any = {
-  txn:{
-    amt: 7,
-    fee: 70224000,
-    fv: 8431331,
-    lv: 8432331,
-    note: [173,116,114,97,110,115,102,101,114,32,109,101,109,111],
-    snd: [168,101,164,68,116,110,137,242,164,216,34,161,122,2,23,221,236,191,81,161,78,114,147,135,226,72,217,99,209,87,1,164],
-    type: 'pay',
-    gen: 'testnet-v1.0',
-    gh: [72,99,181,24,164,179,200,78,200,16,242,45,79,16,129,203,15,113,240,89,167,172,32,222,198,47,127,112,229,9,58,34],
-    rcv: [153,124,244,219,245,145,20,62,130,248,101,46,94,224,204,120,233,96,166,17,57,248,252,37,18,43,238,6,35,95,121,176],
-  },
+  reKeyTo: env.ALGOBETANET_rekeyaccount_A,
 }
 
 async function run() {
@@ -72,8 +62,8 @@ async function run() {
   const transaction = await algoTest.new.Transaction()
 
   // Compose an action from basic parameters using composeAction function
-  const action = await algoTest.composeAction(AlgorandChainActionType.Payment, composeAlgoPaymentParams)
-  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetTransfer, composeTokenTransferParams)
+  const action = await algoTest.composeAction(AlgorandChainActionType.Payment, composeAlgoReKeyParamsA)
+  // const action = await algoTest.composeAction(AlgorandChainActionType.AssetTransfer, composeAlgoReKeyParamsB)
   // OR, set an action using params directly - values depend on the SDK requirements
   // const action = composeAlgoPaymentParams
   transaction.actions = [action]
