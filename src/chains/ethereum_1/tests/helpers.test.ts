c220c3372046de0afa2d4a2677a92a81b46f17b8
Tray Lewin <tray.lewin@live.com>
2020-09-29 10:09:37 -0700

    Eth - Rename toWeiBN

--- src/chains/ethereum_1/tests/helpers.test.ts
+++ src/chains/ethereum_1/tests/helpers.test.ts
@@ -1,13 +1,13 @@
 // How to use fetch mocks - https://www.npmjs.com/package/jest-fetch-mock
 import { ZERO_HEX, EMPTY_HEX, ZERO_ADDRESS } from '../ethConstants'
-import { toWei, toEthereumAddress, toEthereumPrivateKey, ethereumTrxArgIsNullOrEmpty } from '../helpers'
+import { toWeiBN, toEthereumAddress, toEthereumPrivateKey, ethereumTrxArgIsNullOrEmpty } from '../helpers'
 import { EthUnit } from '../models'
 
 describe('Ethereum Helper Functions', () => {
   // sets fetchMock to throw an error on the next call to fetch (jsonRpc.get_abi calls fetch and triggers the error to be thrown)
   it('tests toWei', async () => {
-    const expValue = toWei(100, EthUnit.Milliether)
-    const value = toWei(100, EthUnit.Finney)
+    const expValue = toWeiBN(100, EthUnit.Milliether)
+    const value = toWeiBN(100, EthUnit.Finney)
 
     expect(value).toEqual(expValue)
   })
