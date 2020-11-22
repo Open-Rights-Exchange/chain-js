eefdacae9b536184e26d22a27471544c40687b4b
Tray Lewin <tray.lewin@live.com>
2020-11-22 12:51:31 -0800

    eos - add encryptWithPublicKeys example

--- src/chains/eos_2/examples/crypto.ts
+++ src/chains/eos_2/examples/crypto.ts
@@ -9,6 +9,7 @@
 import { Chain, ChainFactory, ChainType } from '../../../index'
 import { toEosPrivateKey, toEosPublicKey } from '../helpers'
 import { eosPrivateKeyToEccPrivateKey, eosPublicKeyToEccPublicKey } from '../eosCrypto'
+import { toEncryptedDataString } from '../../../crypto/cryptoHelpers'
 
 require('dotenv').config()
 
@@ -25,25 +26,36 @@ const kylinEndpoints = [
 ]
 const chainSettings = {}
 
-async function run() {
+const publicKey1 = toEosPublicKey('EOS741pEuhGBr8xrsW1c5rbRfswRkWK89Qz7rM13uUrGA2eJcnGnx')
+const privateKey1 = toEosPrivateKey('5Je8CkQWxvgJvcxmYWiuSxLnL6nCL21DAysAYxx5Rui4N7bwbPy')
+const publicKey2 = toEosPublicKey('EOS6YdAbt4EZddixhtD4aZtpv9sDsSWRiWTptkCTbjaEZ3QaujK93')
+const privateKey2 = toEosPrivateKey('5JuS5NoPKWBkriHNwLiRrsA2SDsv6ogSBdnCTu8v5EkYpNQjArM')
 
+async function run() {
   // Create an EOS chain and call a few functions
   const kylin = new ChainFactory().create(ChainType.EosV2, kylinEndpoints, chainSettings)
   await kylin.connect()
 
   // crypto
   // encrypt/decrypt with password string
-  const encrypted1 = kylin.encryptWithPassword('mystring', 'password', {salt:'mysalt'})
+  const encrypted1 = kylin.encryptWithPassword('mystring', '2233', { salt: 'us62bn 2l0df5j' })
+  const decrypted1 = kylin.decryptWithPassword(encrypted1, '2233', { salt: 'us62bn 2l0df5j' })
+
   console.log('encrypted text:', encrypted1)
-  const decrypted1 = kylin.decryptWithPassword(encrypted1, 'password', {salt:'mysalt'})
+  // const decrypted1 = kylin.decryptWithPassword(encrypted1, 'password', {salt:'mysalt'})
   console.log('decrypted text:', decrypted1)
 
   // asymmetric encrypt/decrypt with publicKey/privateKey
-  const encrypted2 = await kylin.encryptWithPublicKey('text to encrypt', toEosPublicKey('EOS741pEuhGBr8xrsW1c5rbRfswRkWK89Qz7rM13uUrGA2eJcnGnx'))
-  console.log('encrypted text:', encrypted2)
-  const decrypted2 = await kylin.decryptWithPrivateKey(encrypted2, toEosPrivateKey('5Je8CkQWxvgJvcxmYWiuSxLnL6nCL21DAysAYxx5Rui4N7bwbPy'))
-  console.log('decrypted text:', decrypted2)
+  const encrypted2 = await kylin.encryptWithPublicKey('text to encrypt 2', publicKey1)
+  console.log('encrypted text 2:', encrypted2)
+  const decrypted2 = await kylin.decryptWithPrivateKey(encrypted2, privateKey1)
+  console.log('decrypted text 2:', decrypted2)
 
+  // asymmetric encrypt/decrypt wrapping MULTIPLE publicKey/privateKey pairs - wapped with multiple keys
+  const encrypted3 = await kylin.encryptWithPublicKeys('text to encrypt 3', [publicKey1, publicKey2])
+  console.log('encrypted text 3:', encrypted3)
+  const decrypted3 = await kylin.decryptWithPrivateKeys(encrypted3, [privateKey1, privateKey2])
+  console.log('decrypted asymmetric text:', decrypted3)
 }
 
 ;(async () => {
