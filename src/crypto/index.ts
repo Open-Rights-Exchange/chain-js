0aaab9ad3292347129adaf3102185254743e2a04
Tray Lewin <tray.lewin@live.com>
2020-12-21 06:53:32 -0800

    export Asymmetric

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -1,6 +1,7 @@
 import * as AesCrypto from './aesCrypto'
+import * as Asymmetric from './asymmetric'
 import * as EccCrypto from './eccCrypto'
 import * as Ed25519Crypto from './ed25519Crypto'
 import * as CryptoHelpers from './cryptoHelpers'
 
-export { AesCrypto, EccCrypto, Ed25519Crypto, CryptoHelpers }
+export { AesCrypto, Asymmetric, EccCrypto, Ed25519Crypto, CryptoHelpers }
