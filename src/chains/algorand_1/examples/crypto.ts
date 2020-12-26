95e75512a40567e4ede2f5de920676828ffd0200
Tray Lewin <tray.lewin@live.com>
2020-12-26 11:47:18 -0800

    algo - fix linter errors

--- src/chains/algorand_1/examples/crypto.ts
+++ src/chains/algorand_1/examples/crypto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ChainFactory, ChainType } from '../../../index'
 import { toAlgorandPrivateKey, toAlgorandPublicKey } from '../helpers'
