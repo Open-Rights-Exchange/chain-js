6b4fbca79b03e15e209063133b64a99aa8f568e9
Tray Lewin <tray.lewin@live.com>
2021-01-03 12:41:29 -0800

    algo - update examples to support api v2

--- src/chains/algorand_1/examples/asymmetric.ts
+++ src/chains/algorand_1/examples/asymmetric.ts
@@ -1,4 +1,9 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
 import { Asymmetric } from '../../../crypto'
 import { ChainFactory, ChainType } from '../../../index'
 import { toAlgorandPrivateKey, toAlgorandPublicKey } from '../helpers'
@@ -8,24 +13,21 @@ require('dotenv').config()
 const { env } = process
 
 const algoApiKey = env.AGLORAND_API_KEY || 'missing api key'
-const algoMainnetEndpoints = [
-  {
-    url: new URL('https://mainnet-algorand.api.purestake.io/ps1'),
-    options: { headers: [{ 'X-API-Key': algoApiKey }] },
-  },
-]
-const algoTestnetEndpoints = [
-  {
-    url: new URL('https://testnet-algorand.api.purestake.io/ps1'),
-    options: { headers: [{ 'X-API-Key': algoApiKey }] },
-  },
-]
-const algoBetanetEndpoints = [
-  {
-    url: new URL('https://betanet-algorand.api.purestake.io/ps1'),
-    options: { headers: [{ 'X-API-Key': algoApiKey }] },
-  },
-]
+const algoMainnetEndpoints = [{
+  url: new URL('https://mainnet-algorand.api.purestake.io/ps2'),
+  indexerUrl: new URL('https://mainnet-algorand.api.purestake.io/idx2'),
+  options: { headers: [{ 'x-api-key': algoApiKey }] },
+}]
+const algoTestnetEndpoints = [ {
+  url: new URL('https://testnet-algorand.api.purestake.io/ps2'),
+  indexerUrl: new URL('https://testnet-algorand.api.purestake.io/idx2'),
+  options: { headers: [{ 'x-api-key': algoApiKey }] },
+}]
+const algoBetanetEndpoints = [{
+  url: new URL('https://betanet-algorand.api.purestake.io/ps2'),
+  indexerUrl: new URL('https://betanet-algorand.api.purestake.io/idx2'),
+  options: { headers: [{ 'x-api-key': algoApiKey }] },
+}]
 
 async function run() {
   /** Create Algorand chain instance */
