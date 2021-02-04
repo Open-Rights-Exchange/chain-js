5c8ae61ed54b36bcfd76ba7af2e0015ee25ce7ec
Tray Lewin <tray.lewin@live.com>
2021-02-04 15:29:38 -0800

    algo - ActionHelper now encodes/decodes appArgs correctly

--- src/chains/algorand_1/algoAction.ts
+++ src/chains/algorand_1/algoAction.ts
@@ -1,12 +1,17 @@
 import * as algosdk from 'algosdk'
 import {
+  bigIntToUint8Array,
   bufferToString,
   byteArrayToHexString,
+  hasHexPrefix,
   hexStringToByteArray,
+  isANumber,
   isAString,
   isAUint8Array,
+  isBase64Encoded,
   isHexString,
   isNullOrEmpty,
+  removeHexPrefix,
   toBuffer,
 } from '../../helpers'
 import { throwNewError } from '../../errors'
@@ -16,15 +21,9 @@ import {
   AlgorandTxActionSdkEncoded,
   AlgorandTxActionSdkEncodedFields,
 } from './models/transactionModels'
-import { AlgorandTxHeaderParams, AlgorandChainTransactionParamsStruct, AlgorandTxEncodedForChain } from './models'
+import { AlgorandTxHeaderParams, AlgorandChainTransactionParamsStruct } from './models'
 import { ALGORAND_TRX_COMFIRMATION_ROUNDS, ALGORAND_EMPTY_CONTRACT_NAME } from './algoConstants'
-import {
-  encodeAppArgIfHumanReadable,
-  encodedAppArgsToReadable,
-  isAppArgsSdkEncoded,
-  toAlgorandAddressFromRaw,
-  toRawAddressFromAlgoAddr,
-} from './helpers'
+import { toAlgorandAddressFromRaw, toRawAddressFromAlgoAddr } from './helpers'
 
 /** Helper class to ensure transaction actions properties are set correctly
  * Algorand supports these actions:
@@ -39,8 +38,6 @@ import {
  */
 export class AlgorandActionHelper {
   private _rawAction: AlgorandTxActionRaw
-
-  private _actionEncodedForSdk: AlgorandTxEncodedForChain
 
   /** Creates a new Action from a raw action (or Algrorand action replacing hexstrings for Uint8Arrays)
    *  .action property returns action with Uint8Arrays converted to hexstrings
@@ -77,7 +74,6 @@ export class AlgorandActionHelper {
 
   /** Action properties with addresses converted to hexstrings (from Uint8Arrays) */
   public get action(): AlgorandTxAction {
-    console.log('NOTE: ', this.raw.note)
     const returnVal = {
       ...this.raw,
       genesisHash: this.raw.genesisHash ? bufferToString(this.raw.genesisHash, 'base64') : undefined,
@@ -100,7 +96,8 @@ export class AlgorandActionHelper {
       appClearProgram: !isNullOrEmpty(this.raw.appClearProgram)
         ? byteArrayToHexString(this.raw.appClearProgram)
         : undefined,
-      appArgs: !isNullOrEmpty(this.raw.appArgs) ? encodedAppArgsToReadable(this.raw.appArgs) : undefined,
+      // raw appArgs is an array of UInt8Array - since we cant know what type to decode it to, we convert each to a hexstring - with '0x' prefix for clarity
+      appArgs: !isNullOrEmpty(this.raw.appArgs) ? this.decodeRawAppArgsToReadable(this.raw.appArgs) : undefined,
       group: this.raw.group ? bufferToString(this.raw.group) : undefined,
       lease: !isNullOrEmpty(this.raw.lease) ? algosdk.decodeObj(this.raw.lease) : undefined,
       note: !isNullOrEmpty(this.raw.note) ? algosdk.decodeObj(this.raw.note) : undefined,
@@ -128,13 +125,13 @@ export class AlgorandActionHelper {
       to: toRawAddressFromAlgoAddr(action.to) || undefined,
       from: toRawAddressFromAlgoAddr(action.from) || undefined,
       closeRemainderTo: toRawAddressFromAlgoAddr(action.closeRemainderTo) || undefined,
+      appAccounts: !isNullOrEmpty(action.appAccounts) ? action.appAccounts.map(toRawAddressFromAlgoAddr) : undefined,
       assetManager: toRawAddressFromAlgoAddr(action.assetManager) || undefined,
       assetReserve: toRawAddressFromAlgoAddr(action.assetReserve) || undefined,
       assetFreeze: toRawAddressFromAlgoAddr(action.assetFreeze) || undefined,
       assetClawback: toRawAddressFromAlgoAddr(action.assetClawback) || undefined,
       assetRevocationTarget: toRawAddressFromAlgoAddr(action.assetRevocationTarget) || undefined,
       freezeAccount: toRawAddressFromAlgoAddr(action.freezeAccount) || undefined,
-      appAccounts: !isNullOrEmpty(action.appAccounts) ? action.appAccounts.map(toRawAddressFromAlgoAddr) : undefined,
       reKeyTo: toRawAddressFromAlgoAddr(action.reKeyTo) || undefined,
     }
     return raw
@@ -154,10 +151,8 @@ export class AlgorandActionHelper {
       voteKey,
       ...otherParams
     }: AlgorandTxActionSdkEncodedFields & { otherParams?: any } = action
-    const raw: AlgorandTxActionRaw = this.actionToRaw(otherParams as AlgorandTxAction) as AlgorandTxActionRaw
+    const raw: AlgorandTxActionRaw = this.actionToRaw(otherParams as AlgorandTxAction)
     // these fields are already encoded as needed for raw - so we dont want them encoded again
-    raw.appApprovalProgram = appApprovalProgram
-    raw.appArgs = appArgs
     raw.appClearProgram = appClearProgram
     raw.group = group
     raw.lease = lease
@@ -213,14 +208,13 @@ export class AlgorandActionHelper {
   private encodeFieldsForSdk(paramsIn: any) {
     const { appApprovalProgram, appArgs, appClearProgram, group, lease, note, selectionKey, tag, voteKey } = paramsIn
     const params: any = { ...paramsIn }
+    if (!isNullOrEmpty(appArgs)) params.appArgs = this.encodeAppArgsToRaw(appArgs)
     if (!isNullOrEmpty(appApprovalProgram) && isHexString(appApprovalProgram)) {
       params.appApprovalProgram = hexStringToByteArray(appApprovalProgram)
     }
     if (!isNullOrEmpty(appClearProgram) && isHexString(appClearProgram)) {
       params.appClearProgram = hexStringToByteArray(appClearProgram)
     }
-    if (!isNullOrEmpty(appArgs) && !isAppArgsSdkEncoded(appArgs))
-      params.appArgs = appArgs.map((appArg: string | number | Uint8Array) => encodeAppArgIfHumanReadable(appArg))
     if (!isNullOrEmpty(group) && !Buffer.isBuffer(group)) params.group = toBuffer(group)
     if (!isNullOrEmpty(lease) && !isAUint8Array(lease)) params.lease = algosdk.encodeObj(lease)
     if (!isNullOrEmpty(note) && !isAUint8Array(note)) params.note = algosdk.encodeObj(note)
@@ -245,9 +239,7 @@ export class AlgorandActionHelper {
   /** Remove fields from object that are undefined, null, or empty */
   deleteEmptyFields(paramsIn: { [key: string]: any }) {
     const params = paramsIn
-    Object.keys(params).forEach(key => (isNullOrEmpty(params[key]) ? delete params[key] : {}))
-    if (isAppArgsSdkEncoded(params.appArgs) && (params.appArgs as (Uint8Array | string)[]).length === 0)
-      delete params.appArgs
+    Object.keys(params).forEach((key) => (isNullOrEmpty(params[key]) ? delete params[key] : {}))
   }
 
   /** whether action is the native chain 'raw' format */
@@ -270,7 +262,6 @@ export class AlgorandActionHelper {
       !this.isAlgorandTxActionRaw(action) &&
       (isAUint8Array(action.appApprovalProgram) ||
         isAUint8Array(action.appClearProgram) ||
-        isAppArgsSdkEncoded(action.appArgs) ||
         isAUint8Array(action.note) ||
         isAUint8Array(action.lease) ||
         Buffer.isBuffer(action.group) ||
@@ -278,5 +269,35 @@ export class AlgorandActionHelper {
         Buffer.isBuffer(action.tag) ||
         Buffer.isBuffer(action.voteKey))
     )
+  }
+
+  /** Accepts encoded or unencoded AppArgs array
+   *  Converts to encoded AppArgs - array of Uint8Array
+   */
+  encodeAppArgsToRaw(appArgs: (string | number | Uint8Array)[]): Uint8Array[] {
+    if (!appArgs) return []
+    const appArgsEncoded = appArgs.map((appArg: string | number | Uint8Array) => {
+      if (isAUint8Array(appArg)) return appArg as Uint8Array
+      if (hasHexPrefix(appArg)) return new Uint8Array(Buffer.from(removeHexPrefix(appArg as string), 'hex'))
+      if (isBase64Encoded(appArg)) return new Uint8Array(Buffer.from(appArg as string, 'base64'))
+      if (isAString(appArg)) return new Uint8Array(Buffer.from(appArg as string, 'utf8'))
+      if (isANumber(appArg)) return bigIntToUint8Array(appArg as number) // TODO: Confirm this is the right conversion - can we use an existing function for this?
+      return undefined
+    })
+    return appArgsEncoded || []
+  }
+
+  /** Accepts encoded AppArgs - array of Uint8Array
+   *  Converts to unencoded AppArgs - array of hex encoded strings (with '0x' prefix)
+   */
+  decodeRawAppArgsToReadable(appArgs: Uint8Array[]): string[] {
+    if (isNullOrEmpty(appArgs)) return undefined
+    const readable: string[] = appArgs.map((arg) => {
+      if (isAUint8Array(arg)) {
+        return `0x${Buffer.from(arg).toString('hex')}`
+      }
+      return undefined
+    })
+    return readable
   }
 }
