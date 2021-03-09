321bd26d223ee5739aa1fc89d127a5315b2a08db
Tray Lewin <tray.lewin@live.com>
2021-03-09 09:11:25 -0800

    polka - WIP account example

--- src/chains/polkadot_1/examples/account.ts
+++ src/chains/polkadot_1/examples/account.ts
@@ -1,33 +1,51 @@
+import { createSecureContext } from 'tls'
 import { ChainFactory, ChainType } from '../../../index'
 import { PolkadotChainEndpoint } from '../models'
 
 require('dotenv').config()
 
-const { env } = process
-
 const westendEndpoints: PolkadotChainEndpoint[] = [
   {
-    url: 'wss://westend-rpc.polkadot.io'
-  }
+    url: 'wss://westend-rpc.polkadot.io',
+  },
 ]
 
 const createAccountOptions = {
+  // ...
 }
-;(async () => {
+
+async function createAccount(paraChain: Chain) {
   try {
-    const westend = new ChainFactory().create(ChainType.PolkadotV1, westendEndpoints)
-    await westend.connect()
-    const createAccount = westend.new.CreateAccount(createAccountOptions)
+    await paraChain.connect()
+    const createAccount = paraChain.new.CreateAccount(createAccountOptions)
     await createAccount.generateKeysIfNeeded()
     console.log('generatedKeys:', createAccount.generatedKeys)
     console.log('address:', createAccount.accountName)
-    const account = await westend.new.Account('5FkJuxShVBRJRirM3t3k5Y8XyDaxMi1c8hLdBsH2qeAYqAzF')
+    const account = await paraChain.new.Account('5FkJuxShVBRJRirM3t3k5Y8XyDaxMi1c8hLdBsH2qeAYqAzF')
     console.log('account', account)
-    // const { password, salt } = createAccountOptions.newKeysOptions
-    // const decryptedPrivateKey = ropsten.decryptWithPassword(createAccount.generatedKeys.privateKey, password, { salt })
-    // console.log('decrypted privateKey: ', decryptedPrivateKey)
   } catch (error) {
     console.log(error)
   }
+}
+
+async function run() {
+  try {
+    const paraChainA = new ChainFactory().create(ChainType.PolkadotV1, westendEndpoints)
+    const paraChainB = new ChainFactory().create(ChainType.PolkadotV1, westendEndpoints)
+    const accountA = createAccount(paraChainA)
+    console.log('account', accountA)
+    const accountB = createAccount(paraChainB)
+    console.log('account', accountB)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+;(async () => {
+  try {
+    await run()
+  } catch (error) {
+    console.log('Error:', error)
+  }
+  process.exit()
 })()
-process.exit()
