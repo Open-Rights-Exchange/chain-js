d2f9d8f3d242e7edb52484c5833893c94bbb8378
Tray Lewin <tray.lewin@live.com>
2021-04-01 14:08:45 -0700

    update asym example and location

--- src/crypto/examples/asymmetricCustomCipherKey.ts
+++ src/crypto/examples/asymmetricCustomCipherKey.ts
@@ -4,29 +4,34 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
-import { EciesOptions } from '../crypto/asymmetricModels'
-import { decryptWithPrivateKey, encryptWithPublicKey } from '../crypto/asymmetric'
-import { Asymmetric } from '../crypto'
-
+import { CustomAsymmetricScheme, EciesOptions, SymmetricCypherType } from '../asymmetricModels'
+import { decryptWithPrivateKey, encryptWithPublicKey } from '../asymmetric'
+import { Asymmetric } from '..'
 
 require('dotenv').config()
 
-function customHashGenerate( sharedSecret: Buffer | Uint8Array,
+/** This example demonstrates how to create a custom scheme to generate the ciper key and mac key for an asym encryption
+ *  There is some variability in how someone would contruct these keys (e.g. compressed vs uncompressed public key)
+ *  By creating a customHashGenerator() and/or customMacGenerator function, we can control the approach
+ */
+
+function customMessageKeyGenerator( sharedSecret: Buffer | Uint8Array,
   s1: Buffer,
   ephemKeyBuffer: Buffer ) {
 
   const counter1 = Buffer.from('00000001', 'hex')
   const counter2 = Buffer.from('00000002', 'hex')
-
-  const hash = Asymmetric.hashMessage(
-    'sha256',
+  // uses KDF to derive a symmetric encryption and a MAC keys:
+  // Ke || Km = KDF(S || S1)
+  const hash = Asymmetric.generateMessageHash(
+    SymmetricCypherType.Sha256,
     Buffer.concat(
       [sharedSecret, counter1, s1, ephemKeyBuffer],
       sharedSecret.length + s1.length + ephemKeyBuffer.length + counter1.length,
     ),
   )
-  const hash2 = Asymmetric.hashMessage(
-    'sha256',
+  const hash2 = Asymmetric.generateMessageHash(
+    SymmetricCypherType.Sha256,
     Buffer.concat(
       [sharedSecret, counter2, s1, ephemKeyBuffer],
       sharedSecret.length + s1.length + ephemKeyBuffer.length + counter2.length,
@@ -38,15 +43,22 @@ function customHashGenerate( sharedSecret: Buffer | Uint8Array,
   return {cipherKey, macKey}
 }
 
-function customMacGenerate( macKey?: Buffer, s2?: Buffer, cipherText?: Buffer ) {
-
-  const mac = Asymmetric.macMessage(
-    'sha256',
+function customMacGenerator( macKey?: Buffer, s2?: Buffer, cipherText?: Buffer ) {
+  // computes the tag of encrypted message and S2:
+  // d = MAC(Km; c || S2)
+  const mac = Asymmetric.generateMessageMac(
+    SymmetricCypherType.Sha256,
     macKey,
     Buffer.concat([cipherText, s2], cipherText.length + s2.length),
   )
 
   return mac.slice(0, mac.length / 2)
+}
+
+const customAsymScheme: CustomAsymmetricScheme = {
+  scheme: 'asym.chainjs.secp256k1.custom1',
+  customMessageKeyGenerator,
+  customMacGenerator,
 }
 
 async function run() {
@@ -55,11 +67,11 @@ async function run() {
   const publicWrappingKey = '0450863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b23522cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6'
   const options: EciesOptions = {
     curveType: Asymmetric.EciesCurveType.Secp256k1,
-    symmetricCypherType: 'aes-256-ctr',
+    symmetricCypherType: SymmetricCypherType.Aes256Ctr,
     keyFormat: 'compressed',
     s1: 'ore1rngq1jyx',
   }   
-  const encryptResult = encryptWithPublicKey(publicWrappingKey, toEncrypt, options, customHashGenerate, customMacGenerate)
+  const encryptResult = encryptWithPublicKey(publicWrappingKey, toEncrypt, options, customAsymScheme)
 
   const {ephemPublicKey, ciphertext, mac} = encryptResult
 
@@ -68,7 +80,7 @@ async function run() {
   console.log('encrypted payload:', payload)
   console.log('encrypted payload:', encryptResult)
 
-  const decryptedPayload = await decryptWithPrivateKey(encryptResult, '18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725', options, customHashGenerate, customMacGenerate)
+  const decryptedPayload = await decryptWithPrivateKey(encryptResult, '18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725', options, customAsymScheme)
   console.log('decrypted payload:', decryptedPayload)
 }
 
