0a0c64a8a4ec4a32bbb6690753ec8fcb43ae9f88
Tray Lewin <tray.lewin@live.com>
2021-05-20 19:59:56 -0700

    improve typing of ChainJsPlugin

--- src/interfaces/plugin.ts
+++ src/interfaces/plugin.ts
@@ -2,11 +2,13 @@
  * The PlugIn interface declares the high-level interface to create a plugin for ChainJs
  */
 
-import { Chain } from './chain'
+import { ChainState } from './chainState'
 
 export enum PluginType {
   MultiSig = 'multisig',
 }
+
+export type PluginOptions = any
 
 export class ChainJsPlugin {
   /** Plugin name */
@@ -16,16 +18,16 @@ export class ChainJsPlugin {
   public type: PluginType
 
   /** Plugin options */
-  protected _options: any
+  protected _options: PluginOptions
 
   protected _isInitialized: boolean = false
 
   /** Chainstate - will be set automatically when plugin installed - do not set this */
-  public chainState: Chain
+  public chainState: ChainState
 
   /** Initializes plugin using options
    *  If a plugin doesnt need to be inited, then hardcode protected _isInitialized = true */
-  init(options: any) {
+  init(options: PluginOptions) {
     this._options = options
     this._isInitialized = true
   } // eslint-disable-line @typescript-eslint/no-unused-vars
