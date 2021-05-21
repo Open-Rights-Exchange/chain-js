264424c24cf783654ca76ff5e80689f948f13184
Tray Lewin <tray.lewin@live.com>
2021-05-21 13:48:35 -0700

    add strong typing

--- src/chains/ethereum_1/plugins/multisig/ethereumMultisigPlugin.ts
+++ src/chains/ethereum_1/plugins/multisig/ethereumMultisigPlugin.ts
@@ -1,19 +1,45 @@
-import { MultisigPluginTransaction, MultisigPluginCreateAccount } from '../../../../interfaces/plugins/multisig'
+import { PluginType } from '../../../../interfaces/plugin'
+import {
+  MultisigPluginTransaction,
+  MultisigPluginCreateAccount,
+  MultisigPlugin,
+  MultisigPluginOptions,
+} from '../../../../interfaces/plugins/multisig'
+import { EthereumAddress, EthereumPrivateKey, EthereumTransactionAction } from '../../models'
+import { EthereumMultisigRawTransaction } from './gnosisSafeV1/models'
+
+export type EthereumMultisigTransactionOptions = any
+export type EthereumMultisigCreateAccountOptions = any
+
+export interface EthereumMultisigPlugin extends MultisigPlugin {
+  name: string
+
+  type: PluginType
+
+  init(options: MultisigPluginOptions): Promise<void>
+
+  new: {
+    /** Return a new CreateAccount object used to help with creating a new chain account */
+    CreateAccount(options?: EthereumMultisigCreateAccountOptions): Promise<EthereumMultisigPluginCreateAccount>
+    /** Return a chain Transaction object used to compose and send transactions */
+    Transaction(options?: EthereumMultisigTransactionOptions): Promise<EthereumMultisigPluginTransaction>
+  }
+}
 
 export interface EthereumMultisigPluginCreateAccount extends MultisigPluginCreateAccount {
-  init(input: any): Promise<void>
+  init(options: EthereumMultisigCreateAccountOptions): Promise<void>
 
-  options: any
+  options: EthereumMultisigCreateAccountOptions
 
-  owners: string[]
+  owners: EthereumAddress[]
 
   threshold: number
 
   /** Account named used when creating the account */
-  accountName: any
+  accountName: EthereumAddress
 
   /** Compose the transaction action needed to create the account */
-  transactionAction: any
+  transactionAction: EthereumTransactionAction
 
   /** If true, an transaction must be sent to chain to create account - use createAccountTransactionAction for action needed */
   requiresTransaction: boolean
@@ -22,28 +48,28 @@ export interface EthereumMultisigPluginCreateAccount extends MultisigPluginCreat
 }
 
 export interface EthereumMultisigPluginTransaction extends MultisigPluginTransaction {
-  init(input: any): Promise<void>
+  init(options: EthereumMultisigTransactionOptions): Promise<void>
 
   /** Whether transaction has been prepared for signing (has raw body) */
   hasRaw: boolean
 
-  options: any
+  options: EthereumMultisigTransactionOptions // depends on plug-in
 
-  owners: string[]
+  owners: EthereumAddress[]
 
   threshold: number
 
-  missingSignatures: any[]
+  missingSignatures: EthereumAddress[]
 
   /** Raw transaction body
    *  Note: Set via prepareToBeSigned() or setFromRaw() */
-  rawTransaction: any
+  rawTransaction: EthereumMultisigRawTransaction
 
   /** An array of the unique set of authorizations needed for all actions in transaction */
-  requiredAuthorizations: any[]
+  requiredAuthorizations: EthereumAddress[]
 
-  /** Signatures attached to transaction */
-  signatures: any[]
+  /** Ethereum only supports one signature on a transaction so transaction wont ask multisig plugin for signature list - those are data in the contract */
+  // signatures: EthereumSignature[]
 
   /** Add a signature to the set of attached signatures. Automatically de-duplicates values. */
   addSignatures(signature: any[]): void
@@ -51,7 +77,7 @@ export interface EthereumMultisigPluginTransaction extends MultisigPluginTransac
   prepareToBeSigned(trxEncodedForChain: any): Promise<void>
 
   /** Sign the transaction body with private key(s) and add to attached signatures */
-  sign(privateKeys: any[]): Promise<void>
+  sign(privateKeys: EthereumPrivateKey[]): Promise<void>
 
-  validate(): void
+  validate(): Promise<void>
 }
