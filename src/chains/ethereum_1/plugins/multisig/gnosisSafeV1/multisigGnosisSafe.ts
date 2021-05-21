a48d4677d29afa7b6115e7e226b1444d2cbf2f40
Tray Lewin <tray.lewin@live.com>
2021-05-20 20:31:15 -0700

    use chainState.activeEndpoint

--- src/chains/ethereum_1/plugins/multisig/gnosisSafeV1/multisigGnosisSafe.ts
+++ src/chains/ethereum_1/plugins/multisig/gnosisSafeV1/multisigGnosisSafe.ts
@@ -6,16 +6,16 @@ import { throwNewError } from '../../../../../errors'
 import { EthereumMultisigPlugin } from '../ethereumMultisigPlugin'
 
 export class GnosisSafeMultisigPlugin extends ChainJsPlugin implements EthereumMultisigPlugin {
-  public name = 'Gnosis V1 Multisig Plugin'
+  public name = 'Gnosis Multisig Plugin V1'
 
   public type = PluginType.MultiSig
 
-  // No initalization required
+  // This plug-in does not require initalization
   protected _isInitialized: boolean = true
 
-  /** Allows parent class to (re)initialize options */
-  async init(input: any) {
-    super.init(input)
+  async init(options: any) {
+    /** Pass along options so parent class can initialize */
+    super.init(options)
   }
 
   // This is not actually needed because this plugin doesn't need to be initalized, hence isInitialized always true
@@ -27,14 +27,14 @@ export class GnosisSafeMultisigPlugin extends ChainJsPlugin implements EthereumM
 
   public newCreateAccount = async (options?: EthereumGnosisCreateAccountOptions) => {
     this.assertInitialized()
-    const createAccountPlugin = new GnosisSafeMultisigPluginCreateAccount(options, this.chainState?.endpoints[0]?.url)
+    const createAccountPlugin = new GnosisSafeMultisigPluginCreateAccount(options, this.chainState.activeEndpoint.url)
     await createAccountPlugin.init()
     return createAccountPlugin
   }
 
   public newTransaction = async (options?: EthereumGnosisTransactionOptions) => {
     this.assertInitialized()
-    const transactionPlugin = new GnosisSafeMultisigPluginTransaction(options, this.chainState?.endpoints[0]?.url)
+    const transactionPlugin = new GnosisSafeMultisigPluginTransaction(options, this.chainState.activeEndpoint.url)
     await transactionPlugin.init()
     return transactionPlugin
   }
