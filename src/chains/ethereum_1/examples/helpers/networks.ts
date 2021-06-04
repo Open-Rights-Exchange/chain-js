51305c5c20f191976be5b22e4b0cbadd73fa33f0
artmanque <i@xn--rtman-ewa.com>
2021-06-05 03:39:56 +0500

    Add Ropsten Endpoints

--- src/chains/ethereum_1/examples/helpers/networks.ts
+++ src/chains/ethereum_1/examples/helpers/networks.ts
@@ -1,7 +1,7 @@
 import { ChainEthereumV1, ChainFactory, ChainType } from '../../../..'
 import { TxExecutionPriority } from '../../../../models'
 import { EthereumChainEndpoint, EthereumChainSettings } from '../../models'
-
+// goerli
 export const goerliEndpoints: EthereumChainEndpoint[] = [
   {
     url: 'https://goerli.infura.io/v3/fc379c787fde4363b91a61a345e3620a',
@@ -10,6 +10,22 @@ export const goerliEndpoints: EthereumChainEndpoint[] = [
 export const goerliChainOptions: EthereumChainSettings = {
   chainForkType: {
     chainName: 'goerli',
+    hardFork: 'istanbul',
+  },
+  defaultTransactionSettings: {
+    maxFeeIncreasePercentage: 20,
+    executionPriority: TxExecutionPriority.Fast,
+  },
+}
+// ropsten
+export const ropstenEndpoints: EthereumChainEndpoint[] = [
+  {
+    url: 'https://ropsten.infura.io/v3/fc379c787fde4363b91a61a345e3620a',
+  },
+]
+export const ropstenChainOptions: EthereumChainSettings = {
+  chainForkType: {
+    chainName: 'ropsten',
     hardFork: 'istanbul',
   },
   defaultTransactionSettings: {
