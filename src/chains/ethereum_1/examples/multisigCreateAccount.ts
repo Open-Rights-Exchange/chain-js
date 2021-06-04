ab6a9f19320ba293589e79cef19cfad80616a754
artmanque <i@xn--rtman-ewa.com>
2021-06-05 03:40:37 +0500

    Updaet examples to eth_ropsten

--- src/chains/ethereum_1/examples/multisigCreateAccount.ts
+++ src/chains/ethereum_1/examples/multisigCreateAccount.ts
@@ -2,16 +2,22 @@
 /* eslint-disable import/newline-after-import */
 /* eslint-disable max-len */
 
-import { connectChain, goerliChainOptions, goerliEndpoints } from './helpers/networks'
+import {
+  connectChain,
+  // goerliChainOptions,
+  // goerliEndpoints,
+  ropstenChainOptions,
+  ropstenEndpoints,
+} from './helpers/networks'
 
 import { EthereumGnosisMultisigCreateAccountOptions } from '../plugins/multisig/gnosisSafeV1/models'
-import { toEthereumAddress } from '../helpers'
+import { toEthereumAddress, toEthereumPrivateKey } from '../helpers'
 import { GnosisSafeMultisigPlugin } from '../plugins/multisig/gnosisSafeV1/plugin'
 
 require('dotenv').config()
 ;(async () => {
   try {
-    const goerli = await connectChain(goerliEndpoints, goerliChainOptions)
+    const ropsten = await connectChain(ropstenEndpoints, ropstenChainOptions)
     // address with nonce 0: 0x6E94F570f5639bAb0DD3d9ab050CAf1Ad45BB764
     const multisigOptions: EthereumGnosisMultisigCreateAccountOptions = {
       owners: [
@@ -20,24 +26,25 @@ require('dotenv').config()
         toEthereumAddress(process.env.GOERLI_multisigOwner_2),
       ],
       threshold: 2,
-      saltNonce: 1, // you can't create the multisig account more than once unless you increment the nonce (otherwise you'll see a "reason: 'Create2 call failed'"" error from Gnosis)
+      saltNonce: 3, // you can't create the multisig account more than once unless you increment the nonce (otherwise you'll see a "reason: 'Create2 call failed'"" error from Gnosis)
     }
 
     const gnosisSafePlugin = new GnosisSafeMultisigPlugin()
 
     const pluginOptions = {}
-    await goerli.installPlugin(gnosisSafePlugin, pluginOptions) // auto init the plugin
-    // await gnosisSafePlugin.init(pluginOptions)
+    await ropsten.installPlugin(gnosisSafePlugin, pluginOptions) // auto init the plugin
+    await gnosisSafePlugin.init(pluginOptions)
 
-    const createAccount = await goerli.new.CreateAccount({ multisigOptions })
+    const createAccount = await ropsten.new.CreateAccount({ multisigOptions })
     console.log('accountName: ', createAccount.accountName)
 
     if (createAccount.supportsTransactionToCreateAccount) {
       await createAccount.composeTransaction()
       console.log('IsMultisig: ', createAccount.transaction.isMultisig)
+      // Must sign parent Transaction with any of the multisig account private keys - this signer pays the fees
+      await createAccount.transaction.sign([toEthereumPrivateKey(process.env.GOERLI_multisigOwner_1_PRIVATE_KEY)])
       console.log('createAccount.transaction: ', createAccount.transaction.toJson())
-      // await createAccount.transaction.sign([toEthereumPrivateKey(process.env.GOERLI_multisigOwner_1_PRIVATE_KEY)])
-      // console.log('Txresult: ', await createAccount.transaction.send())
+      console.log('Txresult: ', await createAccount.transaction.send())
     }
   } catch (error) {
     console.log(error)
