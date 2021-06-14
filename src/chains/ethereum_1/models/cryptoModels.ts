703534b917261a2be8e6ab5dd59de7774c3612eb
Tray Lewin <tray.lewin@live.com>
2021-06-14 16:33:48 -0700

    add EthereumSignatureString type

--- src/chains/ethereum_1/models/cryptoModels.ts
+++ src/chains/ethereum_1/models/cryptoModels.ts
@@ -13,8 +13,11 @@ export type EthereumPrivateKey = string & PrivateKeyBrand
 /** a public key string - formatted correctly for ethereum */
 export type EthereumPublicKey = string & PublicKeyBrand
 
-/** a signature string - formatted correcly for ethereum */
+/** a native Ethereum ECDSA signature structure */
 export type EthereumSignature = ECDSASignature & SignatureBrand
+
+/** a stringified Ethereum signature - may have diff underlying formats (e.g. multisig) */
+export type EthereumSignatureString = string & SignatureBrand
 
 /** key pair - in the format returned from algosdk */
 export type EthereumKeyPair = {
