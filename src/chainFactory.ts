d3d204076345bdc4a57ce89b627f7af4d50e565e
Code of Light <warrick.fitzgerald+codeoflight@gmail.com>
2021-11-23 12:43:28 -0500

    Remove algorand and tweak PluginFactory

--- src/chainFactory.ts
+++ src/chainFactory.ts
@@ -1,6 +1,6 @@
 //import { ChainEosV2 } from './chains/eos_2/ChainEosV2'
 //import { ChainEthereumV1 } from './chains/ethereum_1/ChainEthereumV1'
-import { ChainAlgorandV1 } from './chains/algorand_1/ChainAlgorandV1'
+//import { ChainAlgorandV1 } from './chains/algorand_1/ChainAlgorandV1'
 import { Chain } from './interfaces'
 import { ChainType, ChainEndpoint } from './models'
 import { throwNewError } from './errors'
@@ -19,7 +19,8 @@ export class ChainFactory{
         //return new ChainEthereumV1(endpoints, settings)
         throwNewError(`Chain type ${chainType} must be used via plugin`)
       case ChainType.AlgorandV1:
-        return new ChainAlgorandV1(endpoints, settings)
+        //return new ChainAlgorandV1(endpoints, settings)
+        throwNewError(`Chain type ${chainType} must be used via plugin`)
       default:
         throwNewError(`Chain type ${chainType} is not supported`)
     }
@@ -28,17 +29,25 @@ export class ChainFactory{
   
 }
 
+// Can't currently add the constructor signature to Chain interface becasue each plugin is currently different
+// new(endpoints: ChainEndpoint[], settings?: any): Chain;
+// Define the high level plugin interface here again to work around this. 
+type Plugin = {
+  new(endpoints: ChainEndpoint[], settings?: any): Chain;
+  chainType: string;
+}
 
 export function PluginChainFactory(
-  plugins: {new(endpoints: ChainEndpoint[], settings?: any): Chain;}[],
+  plugins: Plugin[],
   chainType: ChainType,
   endpoints: ChainEndpoint[],
   settings?: any  
 ) : Chain
 {
   var chain : Chain= null; 
-  plugins.forEach(plugin => {         
-    if(plugin.toString().includes(chainType)) {
+  plugins.forEach(plugin => {   
+
+    if(plugin.chainType.includes(chainType)) {
       chain = new plugin(endpoints, settings)
     }
   });
