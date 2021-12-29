09364c9d0b276e38ee4e13389c164a8637f8ad0e
Tray Lewin <tray.lewin@live.com>
2021-12-29 08:18:45 -0800

    Migrate - delete chains

--- src/chains/ethereum_1/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './cryptoModelHelpers'
-export * from './generalHelpers'
-export * from './generalModelHelpers'
-export * from './transactionHelpers'
--- src/chains/ethereum_1/models/ethStructures.ts
+++
@@ -1,8 +0,0 @@
-import { ChainActionType } from '../../../models'
-import { EthereumChainActionType } from './chainActionTypeModels'
-
-export type EthereumDecomposeReturn = {
-  chainActionType: ChainActionType | EthereumChainActionType
-  args: any
-  partial?: boolean
-}
