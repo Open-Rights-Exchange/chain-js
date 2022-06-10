2038532fea989b6c3c7c18c8870151666fbc7d7d
artmanque <i@xn--rtman-ewa.com>
2022-06-10 15:15:10 +0500

    feat: add supportResources prop to Chain

--- src/interfaces/chain.ts
+++ src/interfaces/chain.ts
@@ -136,6 +136,8 @@ export interface Chain {
   signMessage(data: string | Buffer, privateKey: PrivateKey): any
   /** Whether chain supports ability to get a publicKey from a signature */
   supportsGetPublicKeyFromSignature: boolean
+  /** Whether the chain supports resources */
+  supportsResources: boolean
   /** Verify that a 'personal message' was signed using the given key (signed with the private key for the provided public key)
    * This differs from verifySignedWithPublicKey() because a message might include additional strings appended (as required by chain best-practices) */
   verifySignedMessage(data: string | Buffer, publicKey: PublicKey, signature: Signature): boolean
