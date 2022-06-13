d7bceee4624ff79b99df7d14c265f890886f8705
artmanque <i@xn--rtman-ewa.com>
2022-06-13 23:10:44 +0500

    feat: add prop requiresResources to transaction

--- src/interfaces/transaction.ts
+++ src/interfaces/transaction.ts
@@ -79,6 +79,8 @@ export interface Transaction {
   /** Internally creates Raw Transaction data.
    *  Requires at least one action set. Must be called before sign() */
   prepareToBeSigned(): Promise<void>
+  /** Whether the transaction needs stakes to execute on chain */
+  requiresResources: boolean
   /** Gets estimated cost in chain specific units to execute this transaction (at current chain rates) */
   resourcesRequired(): Promise<TransactionResources>
   /** set the fee that you would like to pay (based on maxFeeIncreasePercentage) */
