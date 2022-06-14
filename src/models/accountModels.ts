39fee091ede816d1aa802a592455d2083638a1a0
artmanque <i@xn--rtman-ewa.com>
2022-06-14 19:38:50 +0500

    update enum ResourceEstimationType | add comments

--- src/models/accountModels.ts
+++ src/models/accountModels.ts
@@ -1,11 +1,10 @@
-
-export enum EstimationType {
-  Exact = 'exact',
-  Estimate = 'estimate',
-  Partial = 'partial'
+export enum ResourceEstimationType {
+  Exact = 'exact', // The resources returned are the deterministic and complete
+  Estimate = 'estimate', // The resources returned are estimated or averaged
+  Partial = 'partial', // Only some calculatable values are returned - the rest is null
 }
 
 export type AccountResources = {
   [key: string]: any
-  estimationType: EstimationType
+  estimationType: ResourceEstimationType
 }
