481b8ae80ec4f75b56d2053ce535f02eb13e5c56
Tray Lewin <tray.lewin@live.com>
2022-06-15 18:10:26 -0700

    rename resources types and cleanup

--- src/models/accountModels.ts
+++ src/models/accountModels.ts
@@ -1,8 +1,4 @@
-export enum ResourceEstimationType {
-  Exact = 'exact', // The resources returned are the deterministic and complete
-  Estimate = 'estimate', // The resources returned are estimated or averaged
-  Partial = 'partial', // Only some calculatable values are returned - the rest is null
-}
+import { ResourceEstimationType } from './generalModels'
 
 export type AccountResources = {
   [key: string]: any
